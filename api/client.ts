04d9f0750e6d380f3a64fbb526c1f07ee1e5b793
Matthew Kwong <matthewkwong2@gmail.com>
2023-06-21 09:09:05 +0800

    feat: create dedicated v3 contentful ENV

--- api/client.ts
+++ api/client.ts
@@ -4,7 +4,7 @@ import "server-only";
 const client = createClient({
   space: process.env.CF_SPACE_ID ?? "",
   accessToken: process.env.CF_DELIVERY_ACCESS_TOKEN ?? "",
-  environment: "develop", // TODO: change to production later on depending on process.env.VERCEL_ENV
+  environment: process.env.VERCEL_ENV === "production" ? "v3" : "develop", // TODO: change to production later on depending on process.env.VERCEL_ENV
 });
 
 export default client.withoutUnresolvableLinks;
