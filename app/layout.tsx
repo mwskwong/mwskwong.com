cf657e952c64006a32c5984502c194351c99b2dd
Matthew Kwong <matthew.kwong@tecpal.com>
2023-07-25 17:52:28 +0800

    Revert "feat: add JSON-LD"
    This reverts commit ceba3b76d2ee0345a6d2f519e3c93fad835c90e6.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,46 +1,23 @@
 import { Analytics } from "@vercel/analytics/react";
 import { Metadata } from "next";
 import { PropsWithChildren } from "react";
-import { Person, WithContext } from "schema-dts";
 
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import SectionDivider from "@/components/section-divider";
 import { linkedin } from "@/constants/contentful-ids";
 import {
-  address,
-  email,
   firstName,
   jobTitles,
   lastName,
-  middleName,
-  phone,
   selfIntroduction,
 } from "@/constants/data";
-import { getPersonalPhoto, getPlatformProfiles } from "@/lib";
+import { getPlatformProfiles } from "@/lib";
 
 import ThemeRegistry from "./theme-registry";
 
 const RootLayout = async ({ children }: PropsWithChildren) => {
   const platformProfiles = await getPlatformProfiles();
-  const personalPhoto = await getPersonalPhoto();
-  const jsonLd: WithContext<Person> = {
-    "@context": "https://schema.org",
-    "@type": "Person",
-    name: `${firstName} ${lastName}`,
-    givenName: firstName,
-    additionalName: middleName,
-    familyName: lastName,
-    nationality: "Chinese",
-    address,
-    jobTitle: jobTitles.join(" & "),
-    email,
-    telephone: phone,
-    url: process.env.NEXT_PUBLIC_URL,
-    sameAs: platformProfiles.map(({ url }) => url),
-    image: personalPhoto,
-  };
-
   return (
     <html lang="en">
       <body>
@@ -53,10 +30,6 @@ const RootLayout = async ({ children }: PropsWithChildren) => {
           {children}
           <SectionDivider sx={{ bgcolor: "background.level1" }} />
           <Footer />
-          <script
-            type="application/ld+json"
-            dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-          />
         </ThemeRegistry>
         <Analytics />
       </body>
@@ -85,8 +58,12 @@ export const metadata: Metadata = {
     type: "website",
   },
   themeColor: "#ffffff",
-  twitter: { card: "summary_large_image" },
-  alternates: { canonical: "/" },
+  twitter: {
+    card: "summary_large_image",
+  },
+  alternates: {
+    canonical: "/",
+  },
 };
 
 export default RootLayout;
