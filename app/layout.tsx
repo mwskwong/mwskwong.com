a9067df2cd592d7386e804b5a62f0ef4817063b1
Matthew Kwong <matthewkwong2@gmail.com>
2023-08-03 18:47:42 +0800

    feat: add theme-color meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,6 +81,7 @@ export const metadata: Metadata = {
     url: "/",
     type: "website",
   },
+  themeColor: "#fbfcfd",
   twitter: { card: "summary_large_image" },
   alternates: { canonical: "/" },
   archives: ["https://v2.mwskwong.com"],
