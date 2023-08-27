e3270bbba977186daee9d50201293990c9a9f9a1
Matthew Kwong <matthewkwong2@gmail.com>
2023-08-27 14:50:05 +0800

    feat: remove JSON+LD

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import { Analytics } from "@vercel/analytics/react";
 import { Metadata } from "next";
 import { FC, PropsWithChildren } from "react";
-import { Article, WithContext } from "schema-dts";
 
 import Footer from "@/components/footer";
 import Header from "@/components/header";
@@ -13,42 +12,12 @@ import {
   selfIntroduction,
 } from "@/constants/content";
 import { linkedin } from "@/constants/contentful-ids";
-import getExperiences from "@/lib/get-experiences";
 import getPlatformProfiles from "@/lib/get-platform-profiles";
 
 import ThemeRegistry from "./theme-registry";
 
-const name = `${firstName} ${lastName}`;
-const title: Metadata["title"] = {
-  default: `${name} - ${headline}`,
-  template: `%s | ${name}`,
-};
-const path = "/";
-
 const RootLayout: FC<PropsWithChildren> = async ({ children }) => {
-  const [platformProfiles, latestJobTitle] = await Promise.all([
-    getPlatformProfiles(),
-    getExperiences().then((experience) => experience[0].jobTitle),
-  ]);
-
-  const jsonLd: WithContext<Article> = {
-    "@context": "https://schema.org",
-    "@type": "Article",
-    headline: title.default,
-    image: `${process.env.NEXT_PUBLIC_URL}/opengraph-image.png`,
-    datePublished: new Date("2019-07-15").toISOString(),
-    dateModified: new Date().toISOString(),
-    mainEntityOfPage: {
-      "@type": "WebPage",
-      "@id": process.env.NEXT_PUBLIC_URL,
-    },
-    author: {
-      "@type": "Person",
-      name,
-      url: process.env.NEXT_PUBLIC_URL,
-      jobTitle: latestJobTitle,
-    },
-  };
+  const platformProfiles = await getPlatformProfiles();
 
   return (
     <html lang="en">
@@ -63,15 +32,18 @@ const RootLayout: FC<PropsWithChildren> = async ({ children }) => {
           <SectionDivider bgcolor="background.level1" />
           <Footer />
         </ThemeRegistry>
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-        />
         <Analytics />
       </body>
     </html>
   );
 };
+
+const name = `${firstName} ${lastName}`;
+const title: Metadata["title"] = {
+  default: `${name} - ${headline}`,
+  template: `%s | ${name}`,
+};
+const path = "/";
 
 export const metadata: Metadata = {
   title,
