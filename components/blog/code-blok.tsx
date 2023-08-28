6cf2ea72059365b936f7a241ad4d0915323d0077
Matthew Kwong <matthew.kwong@tecpal.com>
2023-08-28 10:00:33 +0800

    feat: code block in complete theme customization

--- components/blog/code-blok.tsx
+++ components/blog/code-blok.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import Box from "@mui/joy/Box";
-import { Highlight, themes } from "prism-react-renderer";
+import { useTheme } from "@mui/joy/styles";
+import { Highlight, PrismTheme } from "prism-react-renderer";
 import {
   ComponentProps,
   DetailedHTMLProps,
@@ -10,6 +11,108 @@ import {
   ReactElement,
 } from "react";
 
+const usePrismTheme = (): PrismTheme => {
+  const theme = useTheme();
+  return {
+    plain: {
+      color: theme.vars.palette.text.secondary,
+      backgroundColor: theme.vars.palette.background.level1,
+    },
+    styles: [
+      {
+        types: ["prolog"],
+        style: { color: "rgb(0, 0, 128)" },
+      },
+      {
+        types: ["comment"],
+        style: {
+          color: "rgb(106, 153, 85)",
+        },
+      },
+      {
+        types: ["builtin", "changed", "keyword", "interpolation-punctuation"],
+        style: {
+          color: theme.vars.palette.primary[300],
+        },
+      },
+      {
+        types: ["number", "inserted"],
+        style: {
+          color: theme.vars.palette.success[200],
+        },
+      },
+      {
+        types: ["constant"],
+        style: {
+          color: "rgb(100, 102, 149)",
+        },
+      },
+      {
+        types: ["attr-name", "variable"],
+        style: {
+          color: "rgb(156, 220, 254)",
+        },
+      },
+      {
+        types: ["deleted", "string", "attr-value", "template-punctuation"],
+        style: {
+          color: theme.vars.palette.warning[400],
+        },
+      },
+      {
+        types: ["selector"],
+        style: {
+          color: "rgb(215, 186, 125)",
+        },
+      },
+      {
+        types: ["tag"],
+        style: {
+          color: "rgb(78, 201, 176)",
+        },
+      },
+      {
+        types: ["tag"],
+        languages: ["markup"],
+        style: {
+          color: "rgb(86, 156, 214)",
+        },
+      },
+      {
+        types: ["punctuation", "operator"],
+        style: {
+          color: "rgb(212, 212, 212)",
+        },
+      },
+      {
+        types: ["punctuation"],
+        languages: ["markup"],
+        style: {
+          color: "#808080",
+        },
+      },
+      {
+        types: ["function"],
+        style: {
+          color: theme.vars.palette.warning[200],
+        },
+      },
+      {
+        types: ["class-name"],
+        style: {
+          color: theme.vars.palette.success[400],
+        },
+      },
+      {
+        types: ["char"],
+        style: {
+          color: "rgb(209, 105, 105)",
+        },
+      },
+    ],
+  };
+};
+
 const CodeBlock: FC<
   DetailedHTMLProps<HTMLAttributes<HTMLPreElement>, HTMLPreElement>
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -17,18 +120,19 @@ const CodeBlock: FC<
   const internalCodeElemProps = (
     children as ReactElement<ComponentProps<"code">>
   ).props;
-
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const language = internalCodeElemProps.className!.split("language-")[1];
+  const prismTheme = usePrismTheme();
 
   return (
     <Highlight
       code={internalCodeElemProps.children as string}
-      theme={themes.vsDark}
+      theme={prismTheme}
       language={language}
     >
       {({ style, tokens, getLineProps, getTokenProps }) => (
         <Box
+          data-joy-color-scheme="dark"
           component="pre"
           fontFamily="code"
           fontSize="sm"
@@ -38,6 +142,7 @@ const CodeBlock: FC<
           border={1}
           borderColor="neutral.outlinedBorder"
           p={2}
+          my={2}
           style={style}
           {...props}
         >
