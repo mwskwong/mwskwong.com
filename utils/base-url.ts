d66687870ce12530e36fe1ee6e3ae25d5ee4ae6e
Matthew Kwong <matthew.kwong@tecpal.com>
2023-08-29 11:22:43 +0800

    fix: base url PROD handling

--- utils/base-url.ts
+++ utils/base-url.ts
@@ -1,7 +1,8 @@
 const baseUrl = `https://${
-  process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL ??
-  process.env.NEXT_PUBLIC_PROD_URL ??
-  ""
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "preview" ||
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "development"
+    ? process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL
+    : process.env.NEXT_PUBLIC_PROD_URL
 }`;
 
 export default baseUrl;
