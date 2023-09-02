907e8cd88ad1d7524831d3ee0bbff1500c8807e9
Matthew Kwong <matthewkwong2@gmail.com>
2023-09-02 13:27:18 +0800

    feat: make use of AspectRatio component

--- components/blog/blog-card-image.tsx
+++ components/blog/blog-card-image.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import AspectRatio from "@mui/joy/AspectRatio";
 import { useTheme } from "@mui/joy/styles";
 import mergeSx from "merge-sx";
 import { ComponentProps, FC } from "react";
@@ -14,37 +15,25 @@ const BlogCardImage: FC<SetOptional<ComponentProps<typeof Image>, "alt">> = ({
   const theme = useTheme();
 
   return (
-    <Image
-      role="presentation"
-      alt=""
-      // WORKAROUND: Next.js requires width and height to be specified for remote image
-      // while with sizes specified, they are meaningless in this case meaningless
-      width={0}
-      height={0}
-      sizes={[
-        `${theme.breakpoints.up("lg")} ${
-          theme.breakpoints.values.lg / (12 / 4)
-        }px`,
-        `${theme.breakpoints.up("md")} ${Math.round((4 / 12) * 100)}vw`,
-        `${theme.breakpoints.up("sm")} ${Math.round((6 / 12) * 100)}vw`,
-        "100vw",
-      ]
-        .join(",")
-        .replaceAll("@media ", "")}
-      sx={mergeSx(
-        {
-          width: "100%",
-          height: "auto",
-          borderRadius: "md",
-          border: 1,
-          borderColor: "neutral.outlinedBorder",
-          objectFit: "cover",
-          aspectRatio: "1200/630",
-        },
-        sx,
-      )}
-      {...props}
-    />
+    <AspectRatio ratio="1200/630" variant="outlined" objectFit="cover">
+      <Image
+        role="presentation"
+        alt=""
+        fill
+        sizes={[
+          `${theme.breakpoints.up("lg")} ${
+            theme.breakpoints.values.lg / (12 / 4)
+          }px`,
+          `${theme.breakpoints.up("md")} ${Math.round((4 / 12) * 100)}vw`,
+          `${theme.breakpoints.up("sm")} ${Math.round((6 / 12) * 100)}vw`,
+          "100vw",
+        ]
+          .join(",")
+          .replaceAll("@media ", "")}
+        sx={mergeSx({ width: "100%", height: "auto", objectFit: "cover" }, sx)}
+        {...props}
+      />
+    </AspectRatio>
   );
 };
 
--- components/blog/cover-image.tsx
+++ components/blog/cover-image.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import AspectRatio from "@mui/joy/AspectRatio";
 import { useTheme } from "@mui/joy/styles";
 import mergeSx from "merge-sx";
 import { ComponentProps, FC } from "react";
@@ -14,34 +15,27 @@ const CoverImage: FC<SetOptional<ComponentProps<typeof Image>, "alt">> = ({
   const theme = useTheme();
 
   return (
-    <Image
-      role="presentation"
-      alt=""
-      // WORKAROUND: Next.js requires width and height to be specified for remote image
-      // while with sizes specified, they are meaningless in this case meaningless
-      width={0}
-      height={0}
-      sizes={[
-        `${theme.breakpoints.up("md")} ${theme.breakpoints.values.md}px`,
-        "100vw",
-      ]
-        .join(",")
-        .replaceAll("@media ", "")}
-      priority
-      sx={mergeSx(
-        {
-          width: "100%",
-          height: "auto",
-          borderRadius: "md",
-          border: 1,
-          borderColor: "neutral.outlinedBorder",
-          objectFit: "cover",
-          aspectRatio: "1200/630",
-        },
-        sx,
-      )}
-      {...props}
-    />
+    <AspectRatio
+      ratio="1200/630"
+      variant="outlined"
+      objectFit="cover"
+      sx={{ borderRadius: "md" }}
+    >
+      <Image
+        role="presentation"
+        alt=""
+        fill
+        sizes={[
+          `${theme.breakpoints.up("md")} ${theme.breakpoints.values.md}px`,
+          "100vw",
+        ]
+          .join(",")
+          .replaceAll("@media ", "")}
+        priority
+        sx={mergeSx({ width: "100%", height: "auto" }, sx)}
+        {...props}
+      />
+    </AspectRatio>
   );
 };
 
