0b0172bd7706526fa28dda130859bc28b78d7015
Matthew Kwong <matthewkwong2@gmail.com>
2023-09-06 23:32:58 +0800

    chore: add comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -69,6 +69,7 @@ module.exports = {
     // sort named imports
     'sort-imports': ['warn', { ignoreDeclarationSort: true }],
   },
+  // Next.js file naming convention
   overrides: [
     {
       files: [
