7fca6658b34e35fea4cae578e5fcda26c9072776
Matthew Kwong <matthew.kwong@tecpal.com>
2023-09-07 10:54:41 +0800

    chore: minor code style alignment

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-/** @type {import("next").NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   compiler: {
     emotion: true,
