e3871880e6e074bef3c4110092bfb294f9216bf0
Matthew Kwong <matthewkwong2@gmail.com>
2023-09-09 22:45:11 +0800

    Revert "feat: remove unnecessary styles"
    This reverts commit 1b686ed344dc682dac0b469603b3147bc3a6bf82.

--- src/theme.ts
+++ src/theme.ts
@@ -116,6 +116,11 @@ export const globalStyles = (theme: Theme) =>
       '&::before': { content: "'“'" },
       '&::after': { content: "'”'" },
     },
+    body: {
+      display: 'flex',
+      flexDirection: 'column',
+      minHeight: '100vh',
+    },
     code: {
       ...theme.typography['body-sm'],
       fontFamily: theme.vars.fontFamily.code,
@@ -129,6 +134,7 @@ export const globalStyles = (theme: Theme) =>
     'h2, h3, h4': {
       scrollMarginTop: 'calc(var(--Header-height) + var(--Section-paddingY))',
     },
+    main: { flex: 1 },
     section: { paddingBlock: 'var(--Section-paddingY)' },
     svg: { display: 'block' },
   }) satisfies Interpolation<Theme>;
