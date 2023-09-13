2fee12a794102772d68d5079e090eb836eb2bfe9
Matthew Kwong <matthewkwong2@gmail.com>
2023-09-14 02:31:10 +0800

    feat: update tech stack

---
+++ src/components/icons/prisma.tsx
@@ -0,0 +1,5 @@
+import { siPrisma } from 'simple-icons';
+
+import { createSimpleSvgIcon } from '@/utils/create-simple-svg-icon';
+
+export const Prisma = createSimpleSvgIcon(siPrisma, 'Prisma');
---
+++ src/components/icons/supabase.tsx
@@ -0,0 +1,5 @@
+import { siSupabase } from 'simple-icons';
+
+import { createSimpleSvgIcon } from '@/utils/create-simple-svg-icon';
+
+export const Supabase = createSimpleSvgIcon(siSupabase, 'Supabase');
