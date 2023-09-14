ab18fdeb386ca66fb41b68e8ff88cec8cfa92630
Matthew Kwong <matthewkwong2@gmail.com>
2023-09-14 22:14:36 +0800

    feat: convert more files to using named import

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,21 @@
 'use client';
 
-import { ClickAwayListener } from '@mui/base/ClickAwayListener';
-import { NoSsr } from '@mui/base/NoSsr';
+import { ClickAwayListener, NoSsr } from '@mui/base';
 import {
   CloseRounded,
   DarkModeRounded,
   LightModeRounded,
   MenuRounded,
 } from '@mui/icons-material';
-import Box, { BoxProps } from '@mui/joy/Box';
-import Container from '@mui/joy/Container';
-import IconButton from '@mui/joy/IconButton';
-import Link from '@mui/joy/Link';
-import Stack from '@mui/joy/Stack';
-import { useColorScheme } from '@mui/joy/styles';
+import {
+  Box,
+  BoxProps,
+  Container,
+  IconButton,
+  Link,
+  Stack,
+  useColorScheme,
+} from '@mui/joy';
 import NextLink from 'next/link';
 import { FC, useRef, useState } from 'react';
 
