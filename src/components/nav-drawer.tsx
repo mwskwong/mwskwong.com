e91060de3d9d40e050380008a42713903def6578
Matthew Kwong <matthewkwong2@gmail.com>
2023-09-15 19:07:10 +0800

    feat: update drawer UI

--- src/components/nav-drawer.tsx
+++ src/components/nav-drawer.tsx
@@ -1,19 +1,17 @@
 'use client';
 
 import { MenuRounded } from '@mui/icons-material';
+import Box from '@mui/joy/Box';
 import Drawer from '@mui/joy/Drawer';
 import IconButton, { IconButtonProps } from '@mui/joy/IconButton';
-import Link from '@mui/joy/Link';
 import List from '@mui/joy/List';
 import ListItem from '@mui/joy/ListItem';
 import ListItemButton from '@mui/joy/ListItemButton';
 import ModalClose from '@mui/joy/ModalClose';
-import Stack from '@mui/joy/Stack';
 import NextLink from 'next/link';
 import { FC, useState } from 'react';
 
-import Icon from '@/app/icon.svg';
-import { home, nav } from '@/constants/nav';
+import { nav } from '@/constants/nav';
 
 export interface NavDrawerProps {
   slotProps?: {
@@ -35,42 +33,29 @@ export const NavDrawer: FC<NavDrawerProps> = ({ slotProps }) => {
         <MenuRounded />
       </IconButton>
       <Drawer anchor="right" onClose={() => setOpen(false)} open={open}>
-        <nav>
-          <Stack
-            alignItems="center"
-            borderBottom={1}
-            borderColor="divider"
-            direction="row"
-            justifyContent="space-between"
-            px={2}
-            py={1.5}
-            spacing={1}
+        <ModalClose />
+        <Box component="nav" pb={1} pt={10} px={6}>
+          <List
+            size="lg"
+            sx={{
+              '--List-gap': '8px',
+              '--List-padding': '0px',
+              '--List-radius': 'var(--joy-radius-md)',
+            }}
           >
-            <Link
-              aria-label="Go to home page"
-              component={NextLink}
-              href={home.href}
-            >
-              <Icon width={32} />
-            </Link>
-            <ModalClose sx={{ position: 'unset' }} />
-          </Stack>
-          <List size="lg">
-            {nav
-              .filter(({ id }) => id !== 'home')
-              .map((section) => (
-                <ListItem key={section.href}>
-                  <ListItemButton
-                    component={NextLink}
-                    href={section.href}
-                    onClick={() => setOpen(false)}
-                  >
-                    {section.name}
-                  </ListItemButton>
-                </ListItem>
-              ))}
+            {nav.map((section) => (
+              <ListItem key={section.href}>
+                <ListItemButton
+                  component={NextLink}
+                  href={section.href}
+                  onClick={() => setOpen(false)}
+                >
+                  {section.name}
+                </ListItemButton>
+              </ListItem>
+            ))}
           </List>
-        </nav>
+        </Box>
       </Drawer>
     </>
   );
