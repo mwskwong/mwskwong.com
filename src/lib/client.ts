3ad2ca1a89e55a5a89cdf12855b63a7ff4115cc5
Matthew Kwong <matthewkwong2@gmail.com>
2023-09-15 11:56:52 +0800

    feat: update contentful ENV var name

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,11 +2,11 @@ import { createClient } from 'contentful';
 import 'server-only';
 
 export const contentful = createClient({
-  space: process.env.CF_SPACE_ID ?? '',
+  space: process.env.CONTENTFUL_SPACE_ID ?? '',
   accessToken:
     (process.env.NODE_ENV === 'development'
-      ? process.env.CF_PREVIEW_ACCESS_TOKEN
-      : process.env.CF_DELIVERY_ACCESS_TOKEN) ?? '',
+      ? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
+      : process.env.CONTENTFUL_ACCESS_TOKEN) ?? '',
   host:
     process.env.NODE_ENV === 'development'
       ? 'preview.contentful.com'
