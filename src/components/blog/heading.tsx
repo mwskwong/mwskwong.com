3568220ccead51911716248f904ef0dca9ce611e
Matthew Kwong <matthewkwong2@gmail.com>
2023-09-21 01:58:45 +0800

    feat: test better MD to HTML conversion

--- src/components/blog/heading.tsx
+++ src/components/blog/heading.tsx
@@ -6,31 +6,27 @@ import Typography, {
   TypographyProps,
   typographyClasses,
 } from '@mui/joy/Typography';
-import { kebabCase } from 'lodash-es';
 import mergeSx from 'merge-sx';
 import { FC } from 'react';
 
 export type HeadingProps = TypographyProps;
 
 // FIXME: with zero runtime, we may be able to make this a server component
-export const Heading: FC<HeadingProps> = ({ sx, ...props }) => {
-  const hash =
-    typeof props.children === 'string' ? kebabCase(props.children) : undefined;
-
+export const Heading: FC<HeadingProps> = ({ id, sx, ...props }) => {
   return (
     <Typography
       endDecorator={
         <Link
-          aria-labelledby={hash}
+          aria-labelledby={id}
           borderRadius="sm"
-          href={`#${hash}`}
+          href={`#${id}`}
           level="body-md"
           variant="outlined"
         >
           <LinkRounded />
         </Link>
       }
-      id={hash}
+      id={id}
       sx={mergeSx(
         {
           [`& .${typographyClasses.endDecorator}`]: {
