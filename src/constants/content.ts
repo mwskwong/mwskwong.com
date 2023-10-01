928544c9a947cea91f70c95b21765d36586cdfb5
Matthew Kwong <matthewkwong2@gmail.com>
2023-10-01 12:51:57 +0800

    chore: commet typo

--- src/constants/content.ts
+++ src/constants/content.ts
@@ -51,7 +51,7 @@ export const websiteTechStack = [
   {
     name: 'MUI Joy UI',
     Icon: Mui,
-    // TODO: change this to https://mui.com/joy-ui once the markeing page is done
+    // TODO: change this to https://mui.com/joy-ui once the marking page is done
     // Issue: https://github.com/mui/material-ui/pull/36829
     url: 'https://mui.com/',
   },
