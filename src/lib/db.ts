23f9b48b899657ae28b3aaa30fc978c4aea6f428
Matthew Kwong <matthewkwong2@gmail.com>
2023-10-05 02:59:38 +0800

    feat: make use of PlanetScale serverless DB driver with Prisma

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,15 @@
 import 'server-only';
 
+import { connect } from '@planetscale/database';
+import { PrismaPlanetScale } from '@prisma/adapter-planetscale';
 import { PrismaClient } from '@prisma/client';
+import { fetch as undiciFetch } from 'undici';
 
-export const prisma = new PrismaClient();
+const connection = connect({
+  host: process.env.DATABASE_HOST,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  fetch: undiciFetch,
+});
+const adapter = new PrismaPlanetScale(connection);
+export const prisma = new PrismaClient({ adapter });
