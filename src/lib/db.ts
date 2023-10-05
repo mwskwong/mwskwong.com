605345d99c11552873742f72d4a07ede59339c9f
Matthew Kwong <matthew.ws.kwong@viu.com>
2023-10-05 13:57:04 +0800

    fix: revert to traditional DB connection

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,5 @@
 import 'server-only';
 
-import { connect } from '@planetscale/database';
-import { PrismaPlanetScale } from '@prisma/adapter-planetscale';
 import { PrismaClient } from '@prisma/client';
 
-const connection = connect({ url: process.env.DATABASE_URL });
-const adapter = new PrismaPlanetScale(connection);
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient();
