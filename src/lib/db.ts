f1575bc1521695c7100f9921eb1ca55cd30533cd
Matthew Kwong <matthew.ws.kwong@viu.com>
2023-10-05 09:38:00 +0800

    refactor: simplify code

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,11 +3,8 @@ import 'server-only';
 import { connect } from '@planetscale/database';
 import { PrismaPlanetScale } from '@prisma/adapter-planetscale';
 import { PrismaClient } from '@prisma/client';
-import { fetch as undiciFetch } from 'undici';
+import { fetch } from 'undici';
 
-const connection = connect({
-  url: process.env.DATABASE_URL,
-  fetch: undiciFetch,
-});
+const connection = connect({ url: process.env.DATABASE_URL, fetch });
 const adapter = new PrismaPlanetScale(connection);
 export const prisma = new PrismaClient({ adapter });
