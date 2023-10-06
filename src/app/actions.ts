0dfb6747d962d0a01f6c247c0ff807a140683472
Matthew Kwong <matthew.ws.kwong@viu.com>
2023-10-06 17:18:16 +0800

    feat: make use of `@planetscale/database`

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,10 +1,16 @@
 'use server';
 
+import { cookies } from 'next/headers';
+
 import { prisma } from '@/lib/db';
 
-export const viewBlogById = async (id: string) =>
-  prisma.blogMetadata.upsert({
+export const viewBlogById = async (id: string) => {
+  // FIXME: @planetscale/database make uses of fetch by default and Next.js is caching this upsert operation as well
+  // calling cookies() to mislead Next.js not to cache it
+  cookies();
+  return prisma.blogMetadata.upsert({
     where: { id },
     update: { view: { increment: 1 } },
     create: { id },
   });
+};
