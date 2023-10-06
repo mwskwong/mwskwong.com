de108a328b8912a18dd70d87030537e01cacdf22
Matthew Kwong <matthew.ws.kwong@viu.com>
2023-10-06 10:57:00 +0800

    feat: make use of planetscale serverless driver and prevent explicit no cache

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,17 @@
 import 'server-only';
 
+import { connect } from '@planetscale/database';
+import { PrismaPlanetScale } from '@prisma/adapter-planetscale';
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+const connection = connect({
+  url: process.env.DATABASE_URL,
+  // WORKAROUND: prevent explicit cache
+  // https://github.com/planetscale/database-js/pull/102#issuecomment-1646372799
+  fetch: (url, init) => {
+    delete init?.cache;
+    return fetch(url, init);
+  },
+});
+const adapter = new PrismaPlanetScale(connection);
+export const prisma = new PrismaClient({ adapter });
