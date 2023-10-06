4f7219a2ba9e455d5c0c6aed0c93b2b6fb9a1218
Matthew Kwong <matthew.ws.kwong@viu.com>
2023-10-06 12:20:44 +0800

    Revert "chore: minor update comment"
    This reverts commit 8b57d5968320c775eca6d6c5271c1b649856c413.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,7 +6,8 @@ import { PrismaClient } from '@prisma/client';
 
 const connection = connect({
   url: process.env.DATABASE_URL,
-  // WORKAROUND: prevent explicit cache https://github.com/planetscale/database-js/pull/102#issuecomment-1646372799
+  // WORKAROUND: prevent explicit cache
+  // https://github.com/planetscale/database-js/pull/102#issuecomment-1646372799
   fetch: (url, init) => {
     delete init?.cache;
     return fetch(url, init);
