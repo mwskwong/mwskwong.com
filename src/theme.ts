9f2064135bc72a06eba4bd1860c03aa1f3447e9f
Matthew Kwong <matthewkwong2@gmail.com>
2023-10-09 11:10:29 +0800

    refactor: use view box to adjust icon scale

--- src/theme.ts
+++ src/theme.ts
@@ -67,7 +67,6 @@ export const globalStyles = (theme: Theme) =>
       '--Section-paddingY': theme.spacing(10),
       '--Footer-paddingY': theme.spacing(6),
       '--Footer-bg': theme.vars.palette.background.surface,
-      '--MaterialIcon-padding': `${(2 / 24).toFixed(5)}em`,
       '--Header-height': '57px',
       '--NProgress-height': '4px',
     },
--- src/utils/create-simple-svg-icon.tsx
+++ src/utils/create-simple-svg-icon.tsx
@@ -1,5 +1,4 @@
 import { SvgIcon, SvgIconProps } from '@mui/joy';
-import mergeSx from 'merge-sx';
 import { FC, forwardRef, memo } from 'react';
 import { SimpleIcon } from 'simple-icons';
 
@@ -7,20 +6,16 @@ export const createSimpleSvgIcon = (
   simpleIcon: SimpleIcon,
   displayName: string,
 ) => {
-  const SimpleSvgIcon: FC<SvgIconProps> = forwardRef(
-    ({ sx, ...props }, ref) => (
-      <SvgIcon
-        htmlColor={
-          props.color === 'branding' ? `#${simpleIcon.hex}` : undefined
-        }
-        ref={ref}
-        sx={mergeSx({ p: 'var(--MaterialIcon-padding)' }, sx)}
-        {...props}
-      >
-        <path d={simpleIcon.path} />
-      </SvgIcon>
-    ),
-  );
+  const SimpleSvgIcon: FC<SvgIconProps> = forwardRef((props, ref) => (
+    <SvgIcon
+      htmlColor={props.color === 'branding' ? `#${simpleIcon.hex}` : undefined}
+      ref={ref}
+      viewBox="-2 -2 28 28"
+      {...props}
+    >
+      <path d={simpleIcon.path} />
+    </SvgIcon>
+  ));
 
   SimpleSvgIcon.displayName = displayName;
   // @ts-expect-error MUI specific checking
