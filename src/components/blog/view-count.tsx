3280c0092bbdbef5ee5a618cbe8180409da36dee
Matthew Kwong <matthewkwong2@gmail.com>
2023-10-25 01:51:16 +0800

    fix: blog metadata should not be cache

--- src/components/blog/view-count.tsx
+++ src/components/blog/view-count.tsx
@@ -3,7 +3,7 @@ import { Typography, TypographyProps } from '@mui/joy';
 import { unstable_noStore as noStore } from 'next/cache';
 import { FC } from 'react';
 
-import { getBlogMetadataById } from '@/lib/get-blog-metadat-by-id';
+import { prisma } from '@/lib/db';
 
 export interface ViewCountProps extends Omit<TypographyProps, 'children'> {
   blogId: string;
@@ -14,7 +14,9 @@ const numberFormatter = new Intl.NumberFormat('en', { notation: 'compact' });
 // TODO: this can be a problem in `/blog` page, since it queries the DB more than it needs
 export const ViewCount: FC<ViewCountProps> = async ({ blogId, ...props }) => {
   noStore();
-  const metadata = await getBlogMetadataById(blogId);
+  const metadata = await prisma.blogMetadata.findUnique({
+    where: { id: blogId },
+  });
 
   return (
     <Typography startDecorator={<VisibilityRounded />} {...props}>
