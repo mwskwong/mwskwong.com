d04f65ad7206b1b7967a7e3fdadd712f38439ad1
Matthew Kwong <matthew.ws.kwong@viu.com>
2023-11-10 14:28:09 +0800

    refactor: make use of satisfies

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,12 +2,13 @@ import { MetadataRoute } from 'next';
 
 import { baseUrl } from '@/constants/base-url';
 
-const robots = (): MetadataRoute.Robots => ({
-  rules: {
-    userAgent: '*',
-    allow: '/',
-  },
-  sitemap: `${baseUrl}/sitemap.xml`,
-});
+const robots = () =>
+  ({
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  }) satisfies MetadataRoute.Robots;
 
 export default robots;
