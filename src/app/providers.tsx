6a862f4930993fc7e571485e4f0e5cdcebd94230
Matthew Kwong <matthew.ws.kwong@viu.com>
2023-11-15 14:48:26 +0800

    feat: remove cache provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -6,7 +6,7 @@ import GlobalStyles from '@mui/joy/GlobalStyles';
 import { CssVarsProvider, getInitColorSchemeScript } from '@mui/joy/styles';
 import { AppProgressBar as NProgressBar } from 'next-nprogress-bar';
 import { FC, PropsWithChildren } from 'react';
-import { NextAppDirEmotionCacheProvider } from 'tss-react/next/appDir';
+// import { NextAppDirEmotionCacheProvider } from 'tss-react/next/appDir';
 
 import { globalStyles, theme } from '@/theme';
 
@@ -14,21 +14,19 @@ export type ProvidersProps = PropsWithChildren;
 export const Providers: FC<ProvidersProps> = ({ children }) => (
   <>
     {getInitColorSchemeScript()}
-    <NextAppDirEmotionCacheProvider options={{ key: 'joy' }}>
-      <CssVarsProvider theme={theme}>
-        <CssBaseline />
-        <GlobalStyles styles={globalStyles} />
-        <NoSsr defer>
-          <NProgressBar
-            color="var(--joy-palette-primary-plainColor)"
-            delay={100}
-            height="var(--NProgress-height)"
-            options={{ showSpinner: false }}
-            shallowRouting
-          />
-        </NoSsr>
-        {children}
-      </CssVarsProvider>
-    </NextAppDirEmotionCacheProvider>
+    <CssVarsProvider theme={theme}>
+      <CssBaseline />
+      <GlobalStyles styles={globalStyles} />
+      <NoSsr defer>
+        <NProgressBar
+          color="var(--joy-palette-primary-plainColor)"
+          delay={100}
+          height="var(--NProgress-height)"
+          options={{ showSpinner: false }}
+          shallowRouting
+        />
+      </NoSsr>
+      {children}
+    </CssVarsProvider>
   </>
 );
