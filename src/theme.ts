e222bb0078adc3282b9d7111843e4e4982e10dae
Matthew Kwong <matthew.ws.kwong@viu.com>
2023-11-21 10:30:49 +0800

    Revert "feat: patch geist to allow options to be customizable"
    This reverts commit 6da430388d02dae1695e0bf5853095b9ea1f7def.

--- src/theme.ts
+++ src/theme.ts
@@ -15,14 +15,11 @@ declare module '@mui/joy/styles' {
   }
 }
 
-const geistSans = GeistSans();
-const geistMono = GeistMono({ preload: false });
-
 export const theme = extendTheme({
   fontFamily: {
-    body: geistSans.style.fontFamily,
-    display: geistSans.style.fontFamily,
-    code: geistMono.style.fontFamily,
+    body: GeistSans.style.fontFamily,
+    display: GeistSans.style.fontFamily,
+    code: GeistMono.style.fontFamily,
   },
   fontSize: {
     xl5: '3rem',
