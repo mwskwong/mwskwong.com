0667093b42a9a5e1e44c2decff19af69576afba1
Matthew Kwong <matthewkwong2@gmail.com>
2023-12-01 00:10:17 +0800

    feat: add delete blog metadata entry API

--- src/app/api/blogs/metadata/route.ts
+++ src/app/api/blogs/metadata/route.ts
@@ -7,14 +7,27 @@ import { BlogSkeleton } from '@/lib/types';
 export const POST = async (request: Request) => {
   const token = headers().get('Authorization')?.split('Bearer ')[1];
 
-  if (token === process.env.INIT_BLOG_METADATA_TOKEN) {
+  if (token === process.env.API_TOKEN) {
     const blog = (await request.json()) as Entry<BlogSkeleton>;
-    // eslint-disable-next-line no-console -- debug
-    console.error(blog);
     const blogMetadata = await prisma.blogMetadata.upsert({
       where: { id: blog.sys.id },
       update: {},
       create: { id: blog.sys.id },
+    });
+
+    return Response.json(blogMetadata);
+  }
+
+  return Response.json(null, { status: 401 });
+};
+
+export const DELETE = async (request: Request) => {
+  const token = headers().get('Authorization')?.split('Bearer ')[1];
+
+  if (token === process.env.API_TOKEN) {
+    const blog = (await request.json()) as Entry<BlogSkeleton>;
+    const blogMetadata = await prisma.blogMetadata.delete({
+      where: { id: blog.sys.id },
     });
 
     return Response.json(blogMetadata);
