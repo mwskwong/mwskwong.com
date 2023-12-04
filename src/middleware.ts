1e61f3806ae987d809fa0ce80a88d7cf00e0fb8a
Matthew Kwong <matthew.ws.kwong@viu.com>
2023-12-04 09:35:35 +0800

    refactor: middleware for clear logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,31 +12,30 @@ const blogSchema = z.object({
 });
 
 const middleware = async (request: NextRequest) => {
-  if (request.nextUrl.pathname.startsWith('/api')) {
-    const authorizationHeader = request.headers.get('Authorization');
-    const accessToken = authorizationHeader?.split('Bearer ')[1];
+  const authorizationHeader = request.headers.get('Authorization');
+  const accessToken = authorizationHeader?.split('Bearer ')[1];
 
-    if (accessToken !== process.env.API_ACCESS_TOKEN) {
+  if (accessToken !== process.env.API_ACCESS_TOKEN) {
+    return NextResponse.json(
+      { message: 'missing or invalid API access token' },
+      { status: 401 },
+    );
+  }
+
+  if (
+    request.nextUrl.pathname.startsWith('/api/blog/metadata') &&
+    request.method.toUpperCase() !== 'GET'
+  ) {
+    const blog = (await request.json()) as Entry<BlogSkeleton>;
+    const result = blogSchema.safeParse(blog);
+    if (!result.success) {
       return NextResponse.json(
-        { message: 'missing or invalid API access token' },
-        { status: 401 },
+        { message: result.error.message },
+        { status: 400 },
       );
-    }
-
-    if (
-      request.nextUrl.pathname.startsWith('/api/blog/metadata') &&
-      request.method.toUpperCase() !== 'GET'
-    ) {
-      const blog = (await request.json()) as Entry<BlogSkeleton>;
-      const result = blogSchema.safeParse(blog);
-      if (!result.success) {
-        return NextResponse.json(
-          { message: result.error.message },
-          { status: 400 },
-        );
-      }
     }
   }
 };
 
+export const config = { matcher: '/api/:path*' };
 export default middleware;
