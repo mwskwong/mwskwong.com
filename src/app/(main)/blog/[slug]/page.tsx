636ba32f2aab4cdddc27f521559fec8d9b1b1ad6
Matthew Kwong <matthew.ws.kwong@viu.com>
2023-12-11 10:00:32 +0800

    feat: improve blog JSON+LD

--- src/app/(main)/blog/[slug]/page.tsx
+++ src/app/(main)/blog/[slug]/page.tsx
@@ -351,6 +351,7 @@ const Blog: FC<BlogProps> = async ({ params: { slug } }) => {
                 image: blog.coverPhoto,
                 datePublished: blog.createdAt,
                 dateModified: blog.updatedAt,
+                url: `${baseUrl}/blog/${slug}`,
                 author: { '@id': person['@id'] },
               } satisfies BlogPosting,
               {
