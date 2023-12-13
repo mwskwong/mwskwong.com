95b9835283f765cebe84f5e5222b31d3ef7923ed
Matthew Kwong <matthew.ws.kwong@viu.com>
2023-12-13 09:38:14 +0800

    fix: remove cache provider for now as it causes styles not being injected in PPR

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,7 +3,7 @@
 import CssBaseline from '@mui/joy/CssBaseline';
 import GlobalStyles from '@mui/joy/GlobalStyles';
 import { CssVarsProvider, getInitColorSchemeScript } from '@mui/joy/styles';
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+// import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import { FC, PropsWithChildren } from 'react';
 
 import { globalStyles, theme } from '@/theme';
@@ -12,12 +12,12 @@ export type ProvidersProps = PropsWithChildren;
 export const Providers: FC<ProvidersProps> = ({ children }) => (
   <>
     {getInitColorSchemeScript()}
-    <AppRouterCacheProvider options={{ key: 'joy' }}>
-      <CssVarsProvider theme={theme}>
-        <CssBaseline />
-        <GlobalStyles styles={globalStyles} />
-        {children}
-      </CssVarsProvider>
-    </AppRouterCacheProvider>
+    {/* <AppRouterCacheProvider options={{ key: 'joy' }}> */}
+    <CssVarsProvider theme={theme}>
+      <CssBaseline />
+      <GlobalStyles styles={globalStyles} />
+      {children}
+    </CssVarsProvider>
+    {/* </AppRouterCacheProvider> */}
   </>
 );
