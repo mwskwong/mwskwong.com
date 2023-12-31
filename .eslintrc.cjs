21e007364a51c732ad679b5852bd3bab9a479926
Matthew Kwong <matthewkwong2@gmail.com>
2023-12-31 22:51:17 +0800

    chore: add eslint config type

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ const { resolve } = require('node:path');
 
 const project = resolve(__dirname, 'tsconfig.json');
 
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
   extends: [
