535e242177b35f34a1d7d5397bcf1feafa7995c1
Matthew Kwong <matthewkwong2@gmail.com>
2024-01-07 18:10:22 +0800

    refactor: better define contact form schema

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -1,25 +1,26 @@
 import { z } from 'zod';
 
+const baseContactFormSchema = z.object({
+  name: z.string().min(1, 'Name is required'),
+  message: z.string().min(1, 'Message is required'),
+});
+
 export const contactFormSchema = z.discriminatedUnion('showInGuestbook', [
-  z.object({
-    name: z.string().min(1, 'Name is required'),
+  baseContactFormSchema.extend({
     email: z
       .string()
       .min(1, 'Email is required')
       .email('Email should be an email'),
     subject: z.string().min(1, 'Subject is required'),
-    message: z.string().min(1, 'Message is required'),
     showInGuestbook: z.literal(false),
   }),
-  z.object({
-    name: z.string().min(1, 'Name is required'),
+  baseContactFormSchema.extend({
     email: z
       .string()
       .email('Email should be an email')
       .optional()
       .or(z.literal('')),
     subject: z.string().optional(),
-    message: z.string().min(1, 'Message is required'),
     showInGuestbook: z.literal(true),
   }),
 ]);
