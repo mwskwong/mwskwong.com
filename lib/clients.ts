a7edcd38e3858963fd2bd6b711adfb26f8f8f10f
Matthew Kwong <matthewkwong2@gmail.com>
2024-01-07 13:09:12 +0800

    feat: remove formspree from tech stack and add Emailjs

--- lib/clients.ts
+++ lib/clients.ts
@@ -4,14 +4,7 @@ import { createClient as createContentfulClient } from 'contentful';
 
 export const contentful = createContentfulClient({
   space: process.env.CONTENTFUL_SPACE_ID ?? '',
-  accessToken:
-    (process.env.NODE_ENV === 'development'
-      ? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
-      : process.env.CONTENTFUL_ACCESS_TOKEN) ?? '',
-  host:
-    process.env.NODE_ENV === 'development'
-      ? 'preview.contentful.com'
-      : undefined,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN ?? '',
   environment: process.env.VERCEL_ENV === 'production' ? 'master' : 'develop',
 }).withoutUnresolvableLinks;
 
