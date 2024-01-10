a0bfc319ff3fa14c708b3f698d6c3b7141a97162
Matthew Kwong <matthew.ws.kwong@viu.com>
2024-01-10 15:00:46 +0800

    fix: optional email condition and error message

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -7,12 +7,13 @@ import {
   object,
   optional,
   string,
+  union,
   variant,
 } from 'valibot';
 
 const baseContactFormSchema = object({
-  name: string([minLength(1, 'Please enter your name.')]),
-  message: string([minLength(1, 'Please enter the message.')]),
+  name: string([minLength(1, 'Name is required')]),
+  message: string([minLength(1, 'Message is required')]),
 });
 
 export const contactFormSchema = variant('showInGuestbook', [
@@ -20,18 +21,20 @@ export const contactFormSchema = variant('showInGuestbook', [
     baseContactFormSchema,
     object({
       email: string([
-        minLength(1, 'Please enter your email.'),
-        email('Please enter a valid email.'),
+        minLength(1, 'Email is required'),
+        email('Not a valid email'),
       ]),
-      subject: string([minLength(1, 'Please enter the subject.')]),
+      subject: string([minLength(1, 'Subject is required')]),
       showInGuestbook: literal(false),
     }),
   ]),
   merge([
     baseContactFormSchema,
     object({
-      email: optional(string([email('Please enter a valid email.')]), ''),
-      subject: optional(string(), ''),
+      email: optional(
+        union([string([email()]), literal('')], 'Not a valid email'),
+      ),
+      subject: optional(string()),
       showInGuestbook: literal(true),
     }),
   ]),
