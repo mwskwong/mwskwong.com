f0e34ae521366794a3315c951eb5e0f6a20ea2b4
Matthew Kwong <matthewkwong2@gmail.com>
2024-01-14 11:33:37 +0800

    feat: update hero section

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -1,13 +1,17 @@
 import { SiLinkedin } from '@icons-pack/react-simple-icons';
 import Box, { BoxProps } from '@mui/joy/Box';
 import Button from '@mui/joy/Button';
+import Chip from '@mui/joy/Chip';
 import Container from '@mui/joy/Container';
 import Stack from '@mui/joy/Stack';
 import Typography from '@mui/joy/Typography';
+import { ArrowRight } from 'lucide-react';
+import NextLink from 'next/link';
 import { FC } from 'react';
 
 import { firstName, headline, lastName } from '@/constants/content';
 import { linkedin } from '@/constants/contentful-ids';
+import { blog } from '@/constants/nav';
 import { getCv, getPlatformProfiles } from '@/lib/queries';
 
 export type HeroProps = Omit<BoxProps<'section'>, 'children'>;
@@ -21,36 +25,51 @@ export const Hero: FC<HeroProps> = async (props) => {
 
   return (
     <Box component="section" pt={{ sm: 16 }} {...props}>
-      <Container>
-        <Stack justifyContent="center" spacing={5}>
-          <Stack spacing={2} textAlign="center">
-            <Typography level="body-sm">Hello</Typography>
-            <Typography level="h1">
-              {"I'm "}
-              <Typography color="primary">{firstName}</Typography> {lastName}
-            </Typography>
-            <Typography level="title-lg">{headline}</Typography>
-          </Stack>
-          <Stack
-            direction={{ xs: 'column', sm: 'row' }}
-            justifyContent="center"
-            spacing={2}
+      <Container sx={{ textAlign: 'center' }}>
+        <Stack alignItems="center" spacing={2}>
+          <Chip
+            component={NextLink}
+            endDecorator={
+              <Typography
+                color="primary"
+                endDecorator={<ArrowRight />}
+                fontWeight="lg"
+                level="title-sm"
+              >
+                Read more
+              </Typography>
+            }
+            href={blog.pathname}
+            variant="outlined"
           >
-            <Button component="a" href={cv} size="lg" target="_blank">
-              Download CV
-            </Button>
-            <Button
-              color="neutral"
-              component="a"
-              href={linkedinProfile?.url}
-              size="lg"
-              startDecorator={<SiLinkedin />}
-              target="_blank"
-              variant="outlined"
-            >
-              {linkedinProfile?.platform?.name}
-            </Button>
-          </Stack>
+            I started writing articles.
+          </Chip>
+          <Typography level="h1">
+            {"I'm "}
+            <Typography color="primary">{firstName}</Typography> {lastName}
+          </Typography>
+          <Typography level="title-lg">{headline}</Typography>
+        </Stack>
+        <Stack
+          direction={{ xs: 'column', sm: 'row' }}
+          justifyContent="center"
+          mt={6}
+          spacing={2}
+        >
+          <Button component="a" href={cv} size="lg" target="_blank">
+            Download CV
+          </Button>
+          <Button
+            color="neutral"
+            component="a"
+            href={linkedinProfile?.url}
+            size="lg"
+            startDecorator={<SiLinkedin />}
+            target="_blank"
+            variant="outlined"
+          >
+            {linkedinProfile?.platform?.name}
+          </Button>
         </Stack>
       </Container>
     </Box>
