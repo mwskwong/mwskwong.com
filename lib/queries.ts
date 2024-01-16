b1d5594c218b5c4cb68ee889869052bde8472d1e
Matthew Kwong <matthewkwong2@gmail.com>
2024-01-16 19:56:50 +0800

    feat: use SSG in `/guestbook` and revalidate when there are new submission

--- lib/queries.ts
+++ lib/queries.ts
@@ -289,10 +289,12 @@ export const getBlogMetadataById = (id: string) => {
   return prisma.blogMetadata.findUnique({ where: { id } });
 };
 
-export const getGuestbookSubmissions = () => {
-  noStore();
-  return prisma.contactFormSubmission.findMany({
-    where: { showInGuestbook: true },
-    orderBy: { submittedAt: 'desc' },
-  });
-};
+export const getGuestbookSubmissions = cache(
+  () =>
+    prisma.contactFormSubmission.findMany({
+      where: { showInGuestbook: true },
+      orderBy: { submittedAt: 'desc' },
+    }),
+  ['guestbook-submissions'],
+  { tags: ['guestbook-submissions'] },
+);
