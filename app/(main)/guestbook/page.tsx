f1635840d41f93574a085b49235ebad62806e0c5
Matthew Kwong <matthew.ws.kwong@viu.com>
2024-01-19 14:49:12 +0800

    feat: update JSON+LD

--- app/(main)/guestbook/page.tsx
+++ app/(main)/guestbook/page.tsx
@@ -2,7 +2,7 @@ import { Button, Container, Stack, Typography } from '@mui/joy';
 import { Metadata } from 'next';
 import Link from 'next/link';
 import { FC, Suspense } from 'react';
-import { BreadcrumbList, Graph } from 'schema-dts';
+import { BreadcrumbList, WithContext } from 'schema-dts';
 
 import {
   SubmissionList,
@@ -11,75 +11,65 @@ import {
 import { SectionDivider } from '@/components/section-divider';
 import { baseUrl } from '@/constants/base-url';
 import { contactForm, guestbook } from '@/constants/nav';
-import { getJsonLdPerson } from '@/lib/utils';
 
 const description =
   'Drop a line in my guestbook. Share your thoughts, stories, or a simple hello.';
 
-const Guestbook: FC = async () => {
-  const person = await getJsonLdPerson();
-
-  return (
-    <>
-      <Container
-        component="main"
-        maxWidth="md"
-        sx={{ py: 'var(--Section-paddingY)' }}
-      >
-        <Stack spacing={8}>
-          <Stack spacing={2} textAlign="center">
-            <Typography level="h1">Guestbook</Typography>
-            <Typography>{description}</Typography>
-          </Stack>
-          <Button
-            component={Link}
-            href={{
-              pathname: contactForm.pathname,
-              hash: contactForm.id,
-              query: { showInGuestbook: true },
-            }}
-            size="lg"
-            sx={{ alignSelf: { sm: 'center' } }}
-          >
-            Leave A Message
-          </Button>
-          <Suspense fallback={<SubmissionListSkeleton />}>
-            <SubmissionList />
-          </Suspense>
+const Guestbook: FC = () => (
+  <>
+    <Container
+      component="main"
+      maxWidth="md"
+      sx={{ py: 'var(--Section-paddingY)' }}
+    >
+      <Stack spacing={8}>
+        <Stack spacing={2} textAlign="center">
+          <Typography level="h1">Guestbook</Typography>
+          <Typography>{description}</Typography>
         </Stack>
-      </Container>
-      <SectionDivider bgcolor="var(--Footer-bg)" />
-      <script
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            '@context': 'https://schema.org',
-            '@graph': [
-              {
-                '@type': 'BreadcrumbList',
-                itemListElement: [
-                  {
-                    '@type': 'ListItem',
-                    name: 'Home',
-                    item: baseUrl,
-                    position: 1,
-                  },
-                  {
-                    '@type': 'ListItem',
-                    name: 'Guestbook',
-                    position: 2,
-                  },
-                ],
-                name: 'Breadcrumbs',
-              } satisfies BreadcrumbList,
-              person,
-            ],
-          } satisfies Graph),
-        }}
-        type="application/ld+json"
-      />
-    </>
-  );
-};
+        <Button
+          component={Link}
+          href={{
+            pathname: contactForm.pathname,
+            hash: contactForm.id,
+            query: { showInGuestbook: true },
+          }}
+          size="lg"
+          sx={{ alignSelf: { sm: 'center' } }}
+        >
+          Leave A Message
+        </Button>
+        <Suspense fallback={<SubmissionListSkeleton />}>
+          <SubmissionList />
+        </Suspense>
+      </Stack>
+    </Container>
+    <SectionDivider bgcolor="var(--Footer-bg)" />
+    <script
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify({
+          '@context': 'https://schema.org',
+          '@type': 'BreadcrumbList',
+          itemListElement: [
+            {
+              '@type': 'ListItem',
+              name: 'Home',
+              item: baseUrl,
+              position: 1,
+            },
+            {
+              '@type': 'ListItem',
+              name: 'Guestbook',
+              position: 2,
+            },
+          ],
+          name: 'Breadcrumbs',
+        } satisfies WithContext<BreadcrumbList>),
+      }}
+      type="application/ld+json"
+    />
+  </>
+);
 
 export const metadata = {
   title: 'Guestbook',
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,6 +1,6 @@
 import { Box } from '@mui/joy';
 import { FC } from 'react';
-import { BreadcrumbList, Graph } from 'schema-dts';
+import { BreadcrumbList, WithContext } from 'schema-dts';
 
 import { About } from '@/components/home/about';
 import { Contact } from '@/components/home/contact';
@@ -9,7 +9,6 @@ import { Experience } from '@/components/home/experience';
 import { Hero } from '@/components/home/hero';
 import { InterestingFact } from '@/components/home/interesting-fact';
 import { SectionDivider } from '@/components/section-divider';
-import { getJsonLdPerson } from '@/lib/utils';
 
 const bgcolors = {
   hero: 'background.body',
@@ -20,74 +19,65 @@ const bgcolors = {
   contact: 'background.body',
 };
 
-const Home: FC = async () => {
-  const person = await getJsonLdPerson();
+const Home: FC = () => (
+  <>
+    <Box
+      component="main"
+      sx={{
+        '& h2, h3, h4': {
+          scrollMarginTop:
+            'calc(var(--Header-height) + var(--Section-paddingY))',
+        },
+      }}
+    >
+      <Hero bgcolor={bgcolors.hero} />
+      <SectionDivider bgcolor={bgcolors.about} color={bgcolors.hero} />
 
-  return (
-    <>
-      <Box
-        component="main"
-        sx={{
-          '& h2, h3, h4': {
-            scrollMarginTop:
-              'calc(var(--Header-height) + var(--Section-paddingY))',
-          },
-        }}
-      >
-        <Hero bgcolor={bgcolors.hero} />
-        <SectionDivider bgcolor={bgcolors.about} color={bgcolors.hero} />
-
-        <About bgcolor={bgcolors.about} />
-        <SectionDivider
-          bgcolor={bgcolors.interestingFact}
-          color={bgcolors.about}
-        />
-
-        <InterestingFact
-          bgcolor={bgcolors.interestingFact}
-          data-joy-color-scheme="dark"
-        />
-        <SectionDivider
-          bgcolor={bgcolors.experience}
-          color={bgcolors.interestingFact}
-        />
-
-        <Experience color={bgcolors.experience} />
-        <SectionDivider
-          bgcolor={bgcolors.education}
-          color={bgcolors.experience}
-        />
-
-        <Education bgcolor={bgcolors.education} />
-        <SectionDivider bgcolor={bgcolors.contact} color={bgcolors.education} />
-
-        <Contact bgcolor={bgcolors.contact} />
-      </Box>
-      <SectionDivider bgcolor="var(--Footer-bg)" />
-      <script
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            '@context': 'https://schema.org',
-            '@graph': [
-              {
-                '@type': 'BreadcrumbList',
-                itemListElement: [
-                  {
-                    '@type': 'ListItem',
-                    name: 'Home',
-                    position: 1,
-                  },
-                ],
-                name: 'Breadcrumbs',
-              } satisfies BreadcrumbList,
-              person,
-            ],
-          } satisfies Graph),
-        }}
-        type="application/ld+json"
+      <About bgcolor={bgcolors.about} />
+      <SectionDivider
+        bgcolor={bgcolors.interestingFact}
+        color={bgcolors.about}
       />
-    </>
-  );
-};
+
+      <InterestingFact
+        bgcolor={bgcolors.interestingFact}
+        data-joy-color-scheme="dark"
+      />
+      <SectionDivider
+        bgcolor={bgcolors.experience}
+        color={bgcolors.interestingFact}
+      />
+
+      <Experience color={bgcolors.experience} />
+      <SectionDivider
+        bgcolor={bgcolors.education}
+        color={bgcolors.experience}
+      />
+
+      <Education bgcolor={bgcolors.education} />
+      <SectionDivider bgcolor={bgcolors.contact} color={bgcolors.education} />
+
+      <Contact bgcolor={bgcolors.contact} />
+    </Box>
+    <SectionDivider bgcolor="var(--Footer-bg)" />
+    <script
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify({
+          '@context': 'https://schema.org',
+          '@type': 'BreadcrumbList',
+          itemListElement: [
+            {
+              '@type': 'ListItem',
+              name: 'Home',
+              position: 1,
+            },
+          ],
+          name: 'Breadcrumbs',
+        } satisfies WithContext<BreadcrumbList>),
+      }}
+      type="application/ld+json"
+    />
+  </>
+);
 
 export default Home;
