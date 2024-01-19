568284a4dec0aa5e84bc368e5c68c5032b089c55
Matthew Kwong <matthew.ws.kwong@viu.com>
2024-01-19 10:49:58 +0800

    fix: rss feed syntax

--- app/(main)/blog/rss.xml/route.ts
+++ app/(main)/blog/rss.xml/route.ts
@@ -1,6 +1,6 @@
 import { baseUrl } from '@/constants/base-url';
 import { email, firstName, lastName } from '@/constants/content';
-import { blog } from '@/constants/nav';
+import { blog, blogRssFeed } from '@/constants/nav';
 import { getBlogs } from '@/lib/queries';
 
 const encodeHtmlEntities = (str: string) =>
@@ -10,12 +10,13 @@ export const GET = async () => {
   const blogs = await getBlogs();
   return new Response(
     `<?xml version="1.0" encoding="UTF-8" ?>
-    <rss version="2.0">     
+    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
     <channel>
       <title>${firstName} ${lastName} Blog</title>
       <link>${baseUrl}${blog.pathname}</link>
       <description>Personal perspectives on a broad range of topics.</description>
       <language>en</language>
+      <atom:link href="${baseUrl}${blogRssFeed.pathname}" rel="self" type="application/rss+xml" />
       ${blogs
         .map(
           ({ title, slug, description, categories = [], createdAt, id }) => `
@@ -23,7 +24,7 @@ export const GET = async () => {
               <title>${title}</title>
               <link>${baseUrl}${blog.pathname}/${slug}</link>
               <description>${encodeHtmlEntities(description)}</description>
-              <guid>${id}</guid>
+              <guid isPermaLink="false">${id}</guid>
               <author>${email} (${firstName} ${lastName})</author>
               ${categories.map((category) => `<category>${category}</category>`).join('')}
               <pubDate>${new Date(createdAt).toUTCString()}</pubDate>
