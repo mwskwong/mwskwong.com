1b61b7e5a06b17506117eb1e4bb44ae41c237a8b
Matthew Kwong <matthew.ws.kwong@viu.com>
2024-01-29 13:29:21 +0800

    feat: add image placeholder for personal photo

--- components/home/about.tsx
+++ components/home/about.tsx
@@ -35,7 +35,10 @@ export const About: FC<AboutProps> = async (props) => {
             {personalPhoto ? (
               <Image
                 alt={`${firstName} ${lastName}`}
+                // 1x1 white
+                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII="
                 height={200}
+                placeholder="blur"
                 priority
                 src={personalPhoto}
                 sx={{
