60120e37df3da295e1cee186af0cc28eb7785698
Matthew Kwong <matthew.ws.kwong@viu.com>
2024-02-07 09:32:06 +0800

    feat: stricter `Referrer-Policy`

--- next.config.mjs
+++ next.config.mjs
@@ -99,7 +99,7 @@ const config = {
         },
         {
           key: 'Referrer-Policy',
-          value: 'origin-when-cross-origin',
+          value: 'strict-origin-when-cross-origin',
         },
       ],
     },
