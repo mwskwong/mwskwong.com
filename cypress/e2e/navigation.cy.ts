a9bd849dde3fa8843bf6cb960d176f28e1608f53
Matthew Kwong <matthew.ws.kwong@viu.com>
2024-02-15 09:59:30 +0800

    test: refactor navigation test

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -8,46 +8,50 @@ describe('Site navigation', () => {
     cy.location('pathname').should('equal', home.pathname);
   });
 
-  describe('Menu button', () => {
-    beforeEach(() => cy.visit('/'));
-    it('shows in mdDown', xs, () => {
-      cy.get('[data-cy="menu-button"]').should('be.visible');
+  describe('Main navigation', () => {
+    beforeEach(() => cy.visit(home.pathname));
+
+    it('shows in drawer ONLY in mdDown', xs, () => {
+      cy.get('[data-cy="nav-list-xs"]').should('not.be.visible');
+      cy.get('[data-cy="menu-button"]').click();
+      cy.get('[data-cy="nav-list-xs"]').should('be.visible');
     });
 
-    it('hides in mdUp', md, () => {
+    it('shows next to logo ONLY in mdUp', md, () => {
+      cy.get('[data-cy="nav-list-md"]').should('be.visible');
       cy.get('[data-cy="menu-button"]').should('not.be.visible');
+      cy.get('[data-cy="nav-list-xs"]').should('not.be.visible');
     });
-  });
 
-  for (const [key, viewport] of [
-    ['xs', xs],
-    ['md', md],
-  ] as const) {
-    describe(`Viewport: ${key}`, viewport, () => {
-      beforeEach(() => {
-        cy.visit(home.pathname);
-        if (key === 'xs') {
-          cy.get('[data-cy="menu-button"]').click();
+    for (const [key, viewport] of [
+      ['xs', xs],
+      ['md', md],
+    ] as const) {
+      describe(`Viewport: ${key}`, viewport, () => {
+        beforeEach(() => {
+          if (key === 'xs') {
+            cy.get('[data-cy="menu-button"]').click();
+          }
+        });
+
+        for (const { id, label, pathname: navPathname } of nav) {
+          it(`navigates to ${label} page/section`, () => {
+            cy.get(`[data-cy="nav-list-${key}"]`).contains(label).click();
+            cy.location().should(({ pathname, hash }) => {
+              expect(pathname).to.eq(navPathname);
+              expect(hash).to.eq(id ? `#${id}` : '');
+            });
+          });
         }
       });
+    }
+  });
 
-      for (const { id, label, pathname: navPathname } of nav) {
-        it(`navigates to ${label} page/section`, () => {
-          cy.get(`[data-cy="nav-list-${key}"]`).contains(label).click();
-          cy.location().should(({ pathname, hash }) => {
-            expect(pathname).to.eq(navPathname);
-            expect(hash).to.eq(id ? `#${id}` : '');
-          });
-        });
-      }
-    });
-  }
-
-  it(`navigates to ${privacyPolicy.label} page`, () => {
+  it(`navigates to ${privacyPolicy.label} page when clicking "Privacy Policy" in footer`, () => {
     cy.visit(home.pathname);
-    // FIXME: we need force = true here because for some reasons,
-    // click event is not triggered (but no error is thrown) if the page scrolls right before it.
-    cy.get('footer').contains(privacyPolicy.label).click({ force: true });
+    // FIXME: we need to use trigger() here because for some reasons,
+    // click() is not triggered (but no error is thrown) if the page scrolls right before it.
+    cy.get('footer').contains(privacyPolicy.label).trigger('click');
     cy.location('pathname').should('equal', privacyPolicy.pathname);
   });
 
