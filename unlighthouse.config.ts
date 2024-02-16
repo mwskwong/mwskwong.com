93f80db3f0e9d3b00747503b12a4e8cbdf9cc636
Matthew Kwong <matthew.ws.kwong@viu.com>
2024-02-16 09:50:54 +0800

    ci(unlighthouse): update config

--- unlighthouse.config.ts
+++ unlighthouse.config.ts
@@ -1,11 +1,11 @@
 export default {
   cache: false,
   ci: {
-    reporter: 'jsonExpanded',
+    buildStatic: true,
     budget: {
       performance: 90,
-      accessibility: 98, // minus point from code blocks some times have color contrast issue
-      'best-practices': 92, // minus point from hydration errors are some times logged
+      accessibility: 98, // minus point from code blocks sometimes have color contrast issue
+      'best-practices': 92, // minus point from hydration errors are sometimes logged due to PPR
       seo: 92, // minus point from non-PROD not being indexable
     },
   },
