f05ccc96ea27c4970b31b3d88b379a191f88db1d
Matthew Kwong <matthew.ws.kwong@viu.com>
2024-02-21 10:11:10 +0800

    test: refactor ctx type definition

--- cypress/e2e/download-cv.cy.ts
+++ cypress/e2e/download-cv.cy.ts
@@ -4,10 +4,12 @@ import { cv } from '../fixtures/contentful-ids';
 import { home } from '../fixtures/nav';
 import { contentful } from '../support/clients';
 
+interface DownloadCvCtx {
+  cv?: Asset<'WITHOUT_UNRESOLVABLE_LINKS'>;
+}
+
 describe('Download CV', () => {
-  const ctx = {
-    cv: undefined as Asset<'WITHOUT_UNRESOLVABLE_LINKS'> | undefined,
-  };
+  const ctx: DownloadCvCtx = {};
 
   before(() => {
     cy.wrap(contentful.getAsset(cv)).then((asset) => {
