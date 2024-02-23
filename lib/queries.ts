c46c48593c9398ece5f54ff41d52bd2d66bd64d2
Matthew Kwong <matthew.ws.kwong@viu.com>
2024-02-23 10:11:04 +0800

    chore: add comments

--- lib/queries.ts
+++ lib/queries.ts
@@ -295,6 +295,7 @@ export const getBlogMetadataById = (id: string) => {
 
 // prevent using Next.js cache to for this despite technically we can + revalidate when new submission happened.
 // this allows moderation on PROD by directly updating the DB
+// Also using React.cache here because both JSON+LD and the UI needs this data
 export const getGuestbookSubmissions = reactCache(() => {
   noStore();
   return prisma.contactFormSubmission.findMany({
