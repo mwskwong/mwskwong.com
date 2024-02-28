3ea9b1c65e321899c6f1540d66fc2518a326868d
Matthew Kwong <matthew.ws.kwong@viu.com>
2024-02-28 09:58:17 +0800

    ci(unlighthouse): adjust budget

--- unlighthouse.config.ts
+++ unlighthouse.config.ts
@@ -4,15 +4,10 @@ export default {
     buildStatic: true,
     budget: {
       // minus point from
-      //   1. TBT due to MUI
-      //   2. LCP due to images
-      // performance: 70,
-      // minus point from
       //   1. Code block doesn't have enough color contrast
-      //   2. Links rely on color to be distinguishable
-      accessibility: 91,
+      accessibility: 98,
       // minus point from hydration errors are sometimes logged due to PPR
-      'best-practices': 96,
+      'best-practices': 92,
       // minus point from non-PROD not being indexable
       seo: 92,
     },
