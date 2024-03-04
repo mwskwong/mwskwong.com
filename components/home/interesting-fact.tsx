20e33689b4a6c180660834fcc99b46d512200b44
Matthew Kwong <matthew.ws.kwong@viu.com>
2024-03-04 11:49:10 +0800

    feat: update interesting fact desc

--- components/home/interesting-fact.tsx
+++ components/home/interesting-fact.tsx
@@ -25,10 +25,8 @@ export const InterestingFact: FC<InterestingFactProps> = async (props) => {
           <Stack spacing={2}>
             <Typography level="h2">Interesting Fact</Typography>
             <Typography maxWidth="sm">
-              My website serves as an experimental platform. It is a space where
-              I actively explore and implement various technologies and new
-              design patterns. You can discover the libraries and platforms
-              utilized in making this site.
+              My website is a testing ground for new tech and design patterns.
+              Check out the libraries and platforms I use to build it.
             </Typography>
           </Stack>
           <Stack
