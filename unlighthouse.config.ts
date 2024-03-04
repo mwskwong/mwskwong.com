b785efa88bed0432240a6dfdfe2d517c36738c42
Matthew Kwong <matthew.ws.kwong@viu.com>
2024-03-04 09:46:27 +0800

    ci(unlighthouse): update options after unlighthouse upgrade

--- unlighthouse.config.ts
+++ unlighthouse.config.ts
@@ -8,22 +8,22 @@ export default {
        *   1. TBT due to MUI
        *   2. LCP due to images, e.g. profile pic and blog cover photo
        */
-      // performance: 80,
+      performance: 90,
       /**
        * Minus point from
        *   1. Code block doesn't have enough color contrast
+       *   2. Links rey on color to be distinguishable (can set underline=always to fix it)
        */
-      accessibility: 98,
-      /**
-       * Minus point from
-       *   1. Hydration mismatch errors are sometimes logged due to PPR
-       */
-      'best-practices': 92,
+      accessibility: 91,
+      'best-practices': 100,
       /**
        * Minus point from
        *   1. Non-PROD not being indexable
        */
       seo: 92,
     },
+  },
+  scanner: {
+    samples: 5,
   },
 };
