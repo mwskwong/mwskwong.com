ad221f0da2fbbac62a44980b657b4a760d582aa9
Matthew Kwong <matthew.ws.kwong@viu.com>
2024-03-06 10:10:54 +0800

    fix: explicit export og image config to avoid build errors

--- app/(main)/guestbook/opengraph-image.tsx
+++ app/(main)/guestbook/opengraph-image.tsx
@@ -1,7 +1,10 @@
 import { guestbook } from '@/constants/nav';
 import { createImage } from '@/og-images/route';
 
-export * from '@/og-images/config';
+// WORKAROUND: explicitly exporting items
+// Although it won;t affect the build result,
+// using export * will causes the error "Attempted import error: 'null' is not exported from 'opengraph-image.tsx?__next_metadata_image_meta__' (imported as '_null')." to throw
+export { size, contentType } from '@/og-images/config';
 export const runtime = 'edge';
 
 export default createImage(guestbook.label);
--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,3 +1,6 @@
 export { Image as default } from '@/og-images/home';
-export * from '@/og-images/config';
+// WORKAROUND: explicitly exporting items
+// Although it won;t affect the build result,
+// using export * will causes the error "Attempted import error: 'null' is not exported from 'opengraph-image.tsx?__next_metadata_image_meta__' (imported as '_null')." to throw
+export { size, contentType } from '@/og-images/config';
 export const runtime = 'edge';
