906cd693379db58ed7ef1b65134d6c7d9c16e379
Matthew Kwong <matthew.ws.kwong@viu.com>
2024-03-15 10:26:32 +0800

    fix: remove unnecessary `useMemo`

--- components/blog/share-dropdown.tsx
+++ components/blog/share-dropdown.tsx
@@ -18,7 +18,7 @@ import {
 } from '@mui/joy';
 import { Share2 } from 'lucide-react';
 import { usePathname } from 'next/navigation';
-import { FC, useMemo } from 'react';
+import { FC } from 'react';
 
 import { firstName, lastName } from '@/constants/content';
 import { baseUrl } from '@/constants/site-config';
@@ -36,32 +36,28 @@ export const ShareDropdown: FC<ShareDropdownProps> = ({ blog, ...props }) => {
 
   const url = `${baseUrl}${pathname}`;
   const text = `"${blog.title}" by ${firstName} ${lastName}`;
-
-  const socialMediaOptions = useMemo(
-    () => [
-      {
-        Icon: SiX,
-        name: 'X',
-        url: `https://twitter.com/intent/tweet?text=${text}&url=${url}&hashtags=${blog.categories?.map((category) => category.replace(' ', '')).join(',')}`,
-      },
-      {
-        Icon: SiFacebook,
-        name: 'Facebook',
-        url: `https://www.facebook.com/sharer/sharer.php?u=${url}`,
-      },
-      {
-        Icon: SiLinkedin,
-        name: 'LinkedIn',
-        url: `https://www.linkedin.com/sharing/share-offsite/?url=${url}`,
-      },
-      {
-        Icon: SiReddit,
-        name: 'Reddit',
-        url: `http://www.reddit.com/submit/?url=${url}&title=${blog.title}`,
-      },
-    ],
-    [blog.categories, blog.title, text, url],
-  );
+  const socialMediaOptions = [
+    {
+      Icon: SiX,
+      name: 'X',
+      url: `https://twitter.com/intent/tweet?text=${text}&url=${url}&hashtags=${blog.categories?.map((category) => category.replace(' ', '')).join(',')}`,
+    },
+    {
+      Icon: SiFacebook,
+      name: 'Facebook',
+      url: `https://www.facebook.com/sharer/sharer.php?u=${url}`,
+    },
+    {
+      Icon: SiLinkedin,
+      name: 'LinkedIn',
+      url: `https://www.linkedin.com/sharing/share-offsite/?url=${url}`,
+    },
+    {
+      Icon: SiReddit,
+      name: 'Reddit',
+      url: `http://www.reddit.com/submit/?url=${url}&title=${blog.title}`,
+    },
+  ];
 
   return (
     <Dropdown {...props}>
