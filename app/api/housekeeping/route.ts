ef511db9c0bf406bd0a1b2763e891e227f592eb0
Matthew Kwong <matthew.ws.kwong@viu.com>
2024-03-25 09:49:23 +0800

    refactor: access authorization header directly

--- app/api/housekeeping/route.ts
+++ app/api/housekeeping/route.ts
@@ -3,18 +3,16 @@ import { headers } from 'next/headers';
 import { db } from '@/lib/clients';
 
 export const GET = async () => {
-  const headersList = headers();
+  const authorization = headers().get('Authorization');
   if (
     process.env.CRON_SECRET &&
-    headersList.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`
+    authorization !== `Bearer ${process.env.CRON_SECRET}`
   ) {
     return Response.json({ status: 1 }, { status: 401 });
   }
 
   const tables = await db.$queryRaw<
-    {
-      tablename: string;
-    }[]
+    { tablename: string }[]
   >`SELECT tablename FROM pg_tables WHERE schemaname = 'public' AND tablename <> '_prisma_migrations'`;
   await db.$executeRawUnsafe(
     `VACUUM ANALYZE ${tables.map(({ tablename }) => tablename).join(', ')}`,
