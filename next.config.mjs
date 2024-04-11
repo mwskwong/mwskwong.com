e87d6574fa4646e18686aa7a1a5e1edd234897e7
Matthew Kwong <matthew.ws.kwong@viu.com>
2024-04-11 11:13:17 +0800

    fix: CSP allow Vercel analytic debug script to run

--- next.config.mjs
+++ next.config.mjs
@@ -133,7 +133,7 @@ const config = {
           key: 'Content-Security-Policy',
           value: `
             default-src 'self';
-            script-src 'self' 'unsafe-eval' 'unsafe-inline';
+            script-src 'self' 'unsafe-eval' 'unsafe-inline' va.vercel-scripts.com;
             style-src 'self' 'unsafe-inline';
             img-src 'self' blob: data:;
             font-src 'self';
