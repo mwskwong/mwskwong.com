113f37f4579418353ae2a3aa2b90506a3ab5c0df
Matthew Kwong <matthew.ws.kwong@viu.com>
2024-04-11 11:02:41 +0800

    feat: add `Permissions-Policy` and CSP header

--- next.config.mjs
+++ next.config.mjs
@@ -118,12 +118,31 @@ const config = {
           value: 'SAMEORIGIN',
         },
         {
+          key: 'Permissions-Policy',
+          value: 'camera=(), microphone=(), geolocation=()',
+        },
+        {
           key: 'X-Content-Type-Options',
           value: 'nosniff',
         },
         {
           key: 'Referrer-Policy',
           value: 'strict-origin-when-cross-origin',
+        },
+        {
+          key: 'Content-Security-Policy',
+          value: `
+            default-src 'self';
+            script-src 'self' 'unsafe-eval' 'unsafe-inline';
+            style-src 'self' 'unsafe-inline';
+            img-src 'self' blob: data:;
+            font-src 'self';
+            object-src 'none';
+            base-uri 'self';
+            form-action 'self';
+            frame-ancestors 'none';
+            upgrade-insecure-requests;
+          `.replace(/\n/g, ''),
         },
       ],
     },
