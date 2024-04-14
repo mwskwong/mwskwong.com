2f95e3fcf77f445a3e35e10436fee53f1bbbfe9b
Matthew Kwong <matthewkwong2@gmail.com>
2024-04-14 13:40:56 +0800

    feat: indicate whether the message is public or private in email

--- src/components/emails/contact-form-notification.tsx
+++ src/components/emails/contact-form-notification.tsx
@@ -9,18 +9,36 @@ export interface ContactFormNotificationProps {
   name: string;
   email?: string;
   message: string;
+  showInGuestbook: boolean;
 }
 
 export const ContactFormNotification = ({
   name,
   email,
   message,
+  showInGuestbook,
 }: ContactFormNotificationProps) => (
   <Layout>
     <Text>Hi {firstName},</Text>
     <Text>
-      You got a new message from{' '}
-      {email ? <Link href={`mailto:${email}`}>{name}</Link> : name}:
+      You got a new{' '}
+      <span
+        style={{
+          display: 'inline-flex',
+          alignItems: 'center',
+          borderRadius: '1.5rem',
+          paddingLeft: 8,
+          paddingRight: 8,
+          fontSize: '0.875rem',
+          backgroundColor: showInGuestbook ? '#E3FBE3' : '#FDF0E1',
+          color: showInGuestbook ? '#0A470A' : '#492B08',
+          minHeight: '1.5rem',
+        }}
+      >
+        {showInGuestbook ? 'Public' : 'Private'}
+      </span>{' '}
+      message from {email ? <Link href={`mailto:${email}`}>{name}</Link> : name}
+      :
     </Text>
     <pre style={{ fontFamily: 'sans-serif' }}>
       <blockquote
@@ -46,6 +64,7 @@ ContactFormNotification.PreviewProps = {
       alert("Hello\\nHow are you?");
     </script>
   `),
+  showInGuestbook: false,
 } satisfies ContactFormNotificationProps;
 
 export default ContactFormNotification;
