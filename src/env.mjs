16b053ab0372458b2d790954636ac88ffefdef53
Matthew Kwong <matthew.ws.kwong@viu.com>
2024-04-30 09:30:44 +0800

    feat: align site url behavior with next.js default

--- src/env.mjs
+++ src/env.mjs
@@ -47,11 +47,11 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_VERCEL_ENV
       ? process.env.NEXT_PUBLIC_PROD_URL &&
-        process.env.NEXT_PUBLIC_VERCEL_URL &&
+        process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL &&
         `https://${
           process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
             ? process.env.NEXT_PUBLIC_PROD_URL
-            : process.env.NEXT_PUBLIC_VERCEL_URL
+            : process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL
         }`
       : 'http://localhost:3000',
     NEXT_PUBLIC_SITE_DISPLAY_NAME:
