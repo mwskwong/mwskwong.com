6bdef43839b579d4e88aeb30748953fdd488247d
Matthew Kwong <matthewkwong2@gmail.com>
2024-05-18 09:41:39 +0800

    feat: update ads styles

--- src/components/ads.tsx
+++ src/components/ads.tsx
@@ -3,26 +3,51 @@
 import { Card, type CardProps, styled } from '@mui/joy';
 import { clsx } from 'clsx';
 import { mergeSx } from 'merge-sx';
-import { type ComponentProps, type FC } from 'react';
+import { type ComponentProps, type FC, useEffect } from 'react';
 
 import { env } from '@/env.mjs';
+
+declare global {
+  interface Window {
+    adsbygoogle?: {
+      loaded: boolean;
+      push: (args: Record<string, unknown>) => void;
+    };
+  }
+}
 
 const Ins = styled('ins')();
 type InsProps = ComponentProps<typeof Ins>;
 
 export type DisplayAdProps = InsProps;
-export const DisplayAd: FC<DisplayAdProps> = ({ className, sx, ...props }) => (
-  <Ins
-    className={clsx('adsbygoogle', className)}
-    data-ad-client="ca-pub-4359361226572500"
-    data-ad-format="auto"
-    data-ad-slot="5863003404"
-    data-adtest={env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? undefined : 'on'}
-    data-full-width-responsive="true"
-    sx={mergeSx({ display: 'block', mx: 'auto' }, sx)}
-    {...props}
-  />
-);
+export const DisplayAd: FC<DisplayAdProps> = ({ className, sx, ...props }) => {
+  useEffect(() => {
+    if (window.adsbygoogle?.loaded) {
+      window.adsbygoogle.push({});
+    }
+  }, [window.adsbygoogle?.loaded]);
+
+  return (
+    <Ins
+      className={clsx('adsbygoogle', className)}
+      data-ad-client="ca-pub-4359361226572500"
+      // data-ad-format="auto"
+      data-ad-slot="5863003404"
+      data-full-width-responsive="true"
+      sx={mergeSx(
+        {
+          display: 'block',
+          mx: 'auto',
+          width: '100%',
+          maxWidth: 'sm',
+          height: 56,
+        },
+        sx,
+      )}
+      {...props}
+    />
+  );
+};
 
 export type BlogCardAdProps = CardProps;
 export const BlogCardAd: FC<BlogCardAdProps> = ({ className, ...props }) => (
