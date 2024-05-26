d90891db13816f836cea05e222e04fd8818b3c6e
Matthew Kwong <matthewkwong2@gmail.com>
2024-05-26 11:23:03 +0800

    refactor: move guestbook json+ld to page component

--- src/app/guestbook/page.tsx
+++ src/app/guestbook/page.tsx
@@ -1,7 +1,14 @@
 import { Button, Container, Stack, Typography } from '@mui/joy';
+import { escape } from 'lodash-es';
 import { type Metadata, type ResolvingMetadata } from 'next';
 import Link from 'next/link';
 import { type FC, Suspense } from 'react';
+import {
+  type BreadcrumbList,
+  type Comment,
+  type DiscussionForumPosting,
+  type Graph,
+} from 'schema-dts';
 
 import { ErrorBoundary } from '@/components/error-boundary';
 import {
@@ -10,12 +17,73 @@ import {
   SubmissionListSkeleton,
 } from '@/components/guestbook/submission-list';
 import { SectionDivider } from '@/components/section-divider';
-import { contactForm, guestbook } from '@/constants/nav';
-
-import { JsonLd } from './json-ld';
+import { contactForm, guestbook, home } from '@/constants/nav';
+import { env } from '@/env.mjs';
+import { getPerson } from '@/lib/json-ld';
+import { getGuestbookSubmissions } from '@/lib/queries';
 
 const description =
   'Drop a line in my guestbook. Share your thoughts, stories, or a simple hello.';
+
+const JsonLd: FC = async () => {
+  const [comments, person] = await Promise.all([
+    getGuestbookSubmissions(),
+    getPerson(),
+  ]);
+
+  return (
+    <script
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify({
+          '@context': 'https://schema.org',
+          '@graph': [
+            {
+              '@type': 'DiscussionForumPosting',
+              author: { '@id': person['@id'] },
+              datePublished: new Date(2024, 0, 10).toISOString(),
+              text: description,
+              comment: comments.map(
+                ({ name, submittedAt, message }) =>
+                  ({
+                    '@type': 'Comment',
+                    author: { '@type': 'Person', name },
+                    datePublished: submittedAt.toISOString(),
+                    text: escape(message),
+                  }) satisfies Comment,
+              ),
+              headline: `${env.NEXT_PUBLIC_SITE_DISPLAY_NAME} ${guestbook.label}`,
+              interactionStatistic: {
+                '@type': 'InteractionCounter',
+                interactionType: { '@type': 'CommentAction' },
+                userInteractionCount: comments.length,
+              },
+              url: env.NEXT_PUBLIC_SITE_URL + guestbook.pathname,
+            } satisfies DiscussionForumPosting,
+            {
+              '@type': 'BreadcrumbList',
+              itemListElement: [
+                {
+                  '@type': 'ListItem',
+                  name: home.label,
+                  item: env.NEXT_PUBLIC_SITE_URL,
+                  position: 1,
+                },
+                {
+                  '@type': 'ListItem',
+                  name: guestbook.label,
+                  position: 2,
+                },
+              ],
+              name: 'Breadcrumbs',
+            } satisfies BreadcrumbList,
+            person,
+          ],
+        } satisfies Graph),
+      }}
+      type="application/ld+json"
+    />
+  );
+};
 
 const Guestbook: FC = () => (
   <>
@@ -51,7 +119,7 @@ const Guestbook: FC = () => (
     <SectionDivider sx={{ bgcolor: 'var(--Footer-bg)' }} />
     <ErrorBoundary>
       <Suspense>
-        <JsonLd discussionForumPosting={{ text: description }} />
+        <JsonLd />
       </Suspense>
     </ErrorBoundary>
   </>
