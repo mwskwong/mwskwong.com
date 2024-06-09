f55b6e75deb51bde43b54bad33b0a7b55d245904
Matthew Kwong <matthewkwong2@gmail.com>
2024-06-09 12:14:52 +0800

    fix: load ads

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -8,20 +8,32 @@ import {
 } from '@mui/joy';
 // FIXME: switch to @mui/joy-nextjs when ready
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
-import { type FC, type PropsWithChildren } from 'react';
+import Script from 'next/script';
+import { type FC, type PropsWithChildren, useState } from 'react';
 
+import { AdsContext } from '@/lib/contexts';
 import { globalStyles, theme } from '@/theme';
 
 export type ProvidersProps = PropsWithChildren;
-export const Providers: FC<ProvidersProps> = ({ children }) => (
-  <>
-    {getInitColorSchemeScript()}
-    <AppRouterCacheProvider options={{ key: 'joy' }}>
-      <CssVarsProvider theme={theme}>
-        <CssBaseline />
-        <GlobalStyles styles={globalStyles} />
-        {children}
-      </CssVarsProvider>
-    </AppRouterCacheProvider>
-  </>
-);
+export const Providers: FC<ProvidersProps> = ({ children }) => {
+  const [adsLoaded, setAdsLoaded] = useState(false);
+
+  return (
+    <>
+      {getInitColorSchemeScript()}
+      <AppRouterCacheProvider options={{ key: 'joy' }}>
+        <CssVarsProvider theme={theme}>
+          <CssBaseline />
+          <GlobalStyles styles={globalStyles} />
+          <AdsContext value={{ loaded: adsLoaded }}>{children}</AdsContext>
+        </CssVarsProvider>
+      </AppRouterCacheProvider>
+      <Script
+        async
+        crossOrigin="anonymous"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4359361226572500"
+        onLoad={() => setAdsLoaded(true)}
+      />
+    </>
+  );
+};
--- src/components/ads.tsx
+++ src/components/ads.tsx
@@ -3,9 +3,9 @@
 import { Card, type CardProps, styled } from '@mui/joy';
 import { clsx } from 'clsx';
 import { mergeSx } from 'merge-sx';
-import { type ComponentProps, type FC, useEffect } from 'react';
+import { type ComponentProps, type FC, use, useEffect } from 'react';
 
-import { env } from '@/env.mjs';
+import { AdsContext } from '@/lib/contexts';
 
 declare global {
   interface Window {
@@ -21,27 +21,22 @@ type InsProps = ComponentProps<typeof Ins>;
 
 export type DisplayAdProps = InsProps;
 export const DisplayAd: FC<DisplayAdProps> = ({ className, sx, ...props }) => {
+  const { loaded } = use(AdsContext);
   useEffect(() => {
-    if (window.adsbygoogle?.loaded) {
-      window.adsbygoogle.push({});
+    if (loaded) {
+      window.adsbygoogle?.push({});
     }
-  }, []);
+  }, [loaded]);
 
   return (
     <Ins
       className={clsx('adsbygoogle', className)}
       data-ad-client="ca-pub-4359361226572500"
-      // data-ad-format="auto"
+      data-ad-format="auto"
       data-ad-slot="5863003404"
       data-full-width-responsive="true"
       sx={mergeSx(
-        {
-          display: 'block',
-          mx: 'auto',
-          width: '100%',
-          maxWidth: 'sm',
-          height: 56,
-        },
+        { display: 'inline-block', mx: 'auto', width: '100%', maxWidth: 'lg' },
         sx,
       )}
       {...props}
@@ -50,15 +45,28 @@ export const DisplayAd: FC<DisplayAdProps> = ({ className, sx, ...props }) => {
 };
 
 export type BlogCardAdProps = CardProps;
-export const BlogCardAd: FC<BlogCardAdProps> = ({ className, ...props }) => (
-  <Card
-    className={clsx('adsbygoogle', className)}
-    component="ins"
-    data-ad-client="ca-pub-4359361226572500"
-    data-ad-format="fluid"
-    data-ad-layout-key="-65+bw+q+5+eo"
-    data-ad-slot="4179721808"
-    data-adtest={env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? undefined : 'on'}
-    {...props}
-  />
-);
+export const BlogCardAd: FC<BlogCardAdProps> = ({
+  className,
+  sx,
+  ...props
+}) => {
+  const { loaded } = use(AdsContext);
+  useEffect(() => {
+    if (loaded) {
+      window.adsbygoogle?.push({});
+    }
+  }, [loaded]);
+
+  return (
+    <Card
+      className={clsx('adsbygoogle', className)}
+      component="ins"
+      data-ad-client="ca-pub-4359361226572500"
+      data-ad-format="fluid"
+      data-ad-layout-key="-65+bw+q+5+eo"
+      data-ad-slot="4179721808"
+      sx={mergeSx({ '--Card-padding': '0px', minHeight: 50 }, sx)}
+      {...props}
+    />
+  );
+};
