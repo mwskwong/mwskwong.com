3701d76b1a770b1cd7ab8c345a84e3ff34e65b3f
Matthew Kwong <matthewkwong2@gmail.com>
2024-06-11 21:06:42 +0800

    feat: remove ad context

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -8,34 +8,20 @@ import {
 } from '@mui/joy';
 // FIXME: switch to @mui/joy-nextjs when ready
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
-import Script from 'next/script';
-import { type FC, type PropsWithChildren, useState } from 'react';
+import { type FC, type PropsWithChildren } from 'react';
 
-import { env } from '@/env.mjs';
-import { AdsContext } from '@/lib/contexts';
 import { globalStyles, theme } from '@/theme';
 
 export type ProvidersProps = PropsWithChildren;
-export const Providers: FC<ProvidersProps> = ({ children }) => {
-  const [adsLoaded, setAdsLoaded] = useState(false);
-
-  return (
-    <>
-      {getInitColorSchemeScript()}
-      <AppRouterCacheProvider options={{ key: 'joy' }}>
-        <CssVarsProvider theme={theme}>
-          <CssBaseline />
-          <GlobalStyles styles={globalStyles} />
-          <AdsContext value={{ loaded: adsLoaded }}>{children}</AdsContext>
-        </CssVarsProvider>
-      </AppRouterCacheProvider>
-      <Script
-        async
-        crossOrigin="anonymous"
-        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${env.NEXT_PUBLIC_AD_SENSE_PUBLISHER_ID}`}
-        strategy="lazyOnload"
-        onLoad={() => setAdsLoaded(true)}
-      />
-    </>
-  );
-};
+export const Providers: FC<ProvidersProps> = ({ children }) => (
+  <>
+    {getInitColorSchemeScript()}
+    <AppRouterCacheProvider options={{ key: 'joy' }}>
+      <CssVarsProvider theme={theme}>
+        <CssBaseline />
+        <GlobalStyles styles={globalStyles} />
+        {children}
+      </CssVarsProvider>
+    </AppRouterCacheProvider>
+  </>
+);
