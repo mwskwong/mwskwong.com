6b2fe08dd6f7610dcfd2e71af933be60a7054f3d
Matthew Kwong <matthewkwong2@gmail.com>
2024-07-18 07:59:58 +0800

    chore: Package update

--- src/lib/clients.ts
+++ src/lib/clients.ts
@@ -12,27 +12,11 @@ export const contentful = createClient({
   environment: env.CONTENTFUL_ENVIRONMENT,
 }).withoutUnresolvableLinks;
 
-/**
- * @see {@link https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dev-practices#solution}
- */
-
-const prismaClientSingleton = () =>
-  new PrismaClient({
-    log:
-      env.NODE_ENV === 'production'
-        ? undefined
-        : ['query', 'info', 'warn', 'error'],
-  });
-
-declare global {
-  // eslint-disable-next-line no-var -- needed for defining globalThis.prismaGlobal
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
-}
-
-export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
-
-if (env.NODE_ENV !== 'production') {
-  globalThis.prismaGlobal = prisma;
-}
+export const prisma = new PrismaClient({
+  log:
+    env.NODE_ENV === 'production'
+      ? undefined
+      : ['query', 'info', 'warn', 'error'],
+});
 
 export const resend = new Resend(env.RESEND_API_KEY);
