88a973599f4e81c71b9f2ba42d279c1b26c3dd8a
Matthew Kwong <matthewkwong2@gmail.com>
2024-12-06 10:57:57 +0800

    feat: basic mantine set up

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,9 +1,7 @@
 import vercelPrettierConfig from '@vercel/style-guide/prettier';
 
 /** @type {import('prettier').Config} */
-const config = {
+export default {
   ...vercelPrettierConfig,
   plugins: [...vercelPrettierConfig.plugins, 'prettier-plugin-prisma'],
 };
-
-export default config;
