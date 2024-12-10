479307c7f3d3aced7a691ff74fbd4405300f5a76
Matthew Kwong <matthewkwong2@gmail.com>
2024-12-10 21:46:57 +0800

    feat: prettier and TS setup

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -3,7 +3,11 @@ import vercelPrettierConfig from '@vercel/style-guide/prettier';
 /** @type {import('prettier').Config} */
 const config = {
   ...vercelPrettierConfig,
-  plugins: [...vercelPrettierConfig.plugins, 'prettier-plugin-prisma'],
+  plugins: [
+    ...vercelPrettierConfig.plugins,
+    'prettier-plugin-prisma',
+    'prettier-plugin-tailwindcss',
+  ],
 };
 
 export default config;
