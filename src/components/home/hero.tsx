8739b84492632b9b0aca2cca7e48633bddf26d37
Matthew Kwong <matthewkwong2@gmail.com>
2024-12-15 15:00:39 +0800

    feat: center align hero section when in larger screens

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -9,29 +9,31 @@ export const Hero: FC = async () => {
   const cv = await getCv();
 
   return (
-    <Section>
-      <Heading mb="4" size="9">
-        {firstName} {lastName}
-      </Heading>
-      <Heading as="h2" color="indigo" size="7">
-        {headline}
-      </Heading>
-      <Text as="p" className="max-w-xl" mt="8">
-        I&apos;m passionate about 🧠 innovative technologies, 😍 user experience
-        web, and ♿️ accessibility.
-      </Text>
-      <Flex direction={{ initial: 'column', sm: 'row' }} gap="4" mt="8">
-        <Button asChild highContrast color="gray" size="4">
-          <a href={cv} rel="noopener" target="_blank">
-            Download CV
-          </a>
-        </Button>
-        <Button asChild highContrast size="4" variant="soft">
-          <a href={linkedin} rel="noopener" target="_blank">
-            <IconBrandLinkedin size={18} />
-            LinkedIn
-          </a>
-        </Button>
+    <Section asChild>
+      <Flex align={{ sm: 'center' }} direction="column">
+        <Heading size="9">
+          {firstName} {lastName}
+        </Heading>
+        <Heading as="h2" color="indigo" mt="4" size="7">
+          {headline}
+        </Heading>
+        <Text align={{ sm: 'center' }} as="p" className="max-w-xl" mt="8">
+          I&apos;m passionate about 🧠 innovative technologies, 😍 user
+          experience web, and ♿️ accessibility.
+        </Text>
+        <Flex direction={{ initial: 'column', sm: 'row' }} gap="4" mt="8">
+          <Button asChild highContrast color="gray" size="4">
+            <a href={cv} rel="noopener" target="_blank">
+              Download CV
+            </a>
+          </Button>
+          <Button asChild highContrast size="4" variant="soft">
+            <a href={linkedin} rel="noopener" target="_blank">
+              <IconBrandLinkedin size={18} />
+              LinkedIn
+            </a>
+          </Button>
+        </Flex>
       </Flex>
     </Section>
   );
