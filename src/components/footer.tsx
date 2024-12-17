fd2e28de684ad82c610754e4782d16283403d612
Matthew Kwong <matthewkwong2@gmail.com>
2024-12-18 00:15:46 +0800

    feat: change copyright notice to auto use latest year

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -33,29 +33,39 @@ const links = [
 ];
 
 export type FooterProps = Omit<ContainerProps, 'asChild' | 'children'>;
-export const Footer: FC<FooterProps> = (props) => (
-  <Container asChild {...props}>
-    <footer>
-      <Separator size="4" />
-      <Section>
-        <Flex direction="column" gap="6">
-          <Heading size="7">
-            {firstName} {lastName}
-          </Heading>
-          <Flex gap="5">
-            {links.map(({ Icon, href, name }) => (
-              <IconButton key={href} asChild color="gray" variant="ghost">
-                <a aria-label={name} href={href} rel="noopener" target="_blank">
-                  <Icon size={18} />
-                </a>
-              </IconButton>
-            ))}
+// eslint-disable-next-line @typescript-eslint/require-await -- "use cache" functions must be async functions
+export const Footer: FC<FooterProps> = async (props) => {
+  'use cache';
+
+  return (
+    <Container asChild {...props}>
+      <footer>
+        <Separator size="4" />
+        <Section>
+          <Flex direction="column" gap="6">
+            <Heading size="7">
+              {firstName} {lastName}
+            </Heading>
+            <Flex gap="5">
+              {links.map(({ Icon, href, name }) => (
+                <IconButton key={href} asChild color="gray" variant="ghost">
+                  <a
+                    aria-label={name}
+                    href={href}
+                    rel="noopener"
+                    target="_blank"
+                  >
+                    <Icon size={18} />
+                  </a>
+                </IconButton>
+              ))}
+            </Flex>
           </Flex>
-        </Flex>
-        <Text as="p" color="gray" mt="9">
-          Copyright © 2024 – All right reserved
-        </Text>
-      </Section>
-    </footer>
-  </Container>
-);
+          <Text as="p" color="gray" mt="9">
+            Copyright © {new Date().getFullYear()} – All right reserved
+          </Text>
+        </Section>
+      </footer>
+    </Container>
+  );
+};
