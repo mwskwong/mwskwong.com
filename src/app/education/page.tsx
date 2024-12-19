a7cadb1f4fb974e1fdd2c3f0416328ffd8b32229
Matthew Kwong <matthewkwong2@gmail.com>
2024-12-19 08:18:07 +0800

    feat: add experience and education dummy page

---
+++ src/app/education/page.tsx
@@ -0,0 +1,9 @@
+const EducationPage = () => {
+  return (
+    <div style={{ height: 3000 }}>
+      <h1>Education</h1>
+    </div>
+  );
+};
+
+export default EducationPage;
---
+++ src/app/experience/page.tsx
@@ -0,0 +1,9 @@
+const ExperiencePage = () => {
+  return (
+    <div style={{ height: 3000 }}>
+      <h1>Experience</h1>
+    </div>
+  );
+};
+
+export default ExperiencePage;
