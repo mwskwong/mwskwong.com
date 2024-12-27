80de988f9c3a68ea5c4d8b4b362bbd472d341a47
Matthew Kwong <matthewkwong2@gmail.com>
2024-12-27 10:10:10 +0800

    feat: add blog rss feed

---
+++ src/app/blog/rss.xml/route.ts
@@ -0,0 +1,47 @@
+import { Feed } from 'feed';
+
+import { email, firstName, lastName, legalName } from '@/constants/me';
+import { routes, siteUrl } from '@/constants/site-config';
+import { getArticles } from '@/lib/queries';
+
+const name = `${firstName} ${lastName}`;
+
+export const GET = async () => {
+  const blogs = await getArticles();
+  const blogFeed = new Feed({
+    title: `${name} ${routes.blog.name}`,
+    description: 'Personal perspectives on a broad range of topics.',
+    id: siteUrl + routes.blogRssFeed.pathname,
+    link: siteUrl + routes.blog.pathname,
+    language: 'en',
+    copyright: `Copyright © ${new Date().getFullYear()} ${legalName}`,
+    feedLinks: {
+      rss: siteUrl + routes.blogRssFeed.pathname,
+      atom: 'self',
+    },
+  });
+
+  for (const {
+    title,
+    slug,
+    description,
+    categories = [],
+    createdAt,
+    updatedAt,
+  } of blogs) {
+    blogFeed.addItem({
+      guid: `${siteUrl}${routes.blog.pathname}/${slug}`,
+      title,
+      link: `${siteUrl}${routes.blog.pathname}/${slug}`,
+      description,
+      author: [{ name, email }],
+      published: createdAt,
+      date: updatedAt,
+      category: categories.map((category) => ({ name: category })),
+    });
+  }
+
+  return new Response(blogFeed.rss2(), {
+    headers: { 'Content-Type': 'text/xml' },
+  });
+};
