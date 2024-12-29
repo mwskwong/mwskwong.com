dd1958f3fc101b6bc5db9d39d5b72e31f093e759
Matthew Kwong <matthewkwong2@gmail.com>
2024-12-29 20:06:32 +0800

    fix: server actions must use `async` keywords

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -2,8 +2,8 @@
 
 import { prisma } from './clients';
 
-export const incrementArticleView = (id: string) =>
-  prisma.articleMetadata.upsert({
+export const incrementArticleView = async (id: string) =>
+  await prisma.articleMetadata.upsert({
     where: { id },
     update: { view: { increment: 1 } },
     create: { id },
