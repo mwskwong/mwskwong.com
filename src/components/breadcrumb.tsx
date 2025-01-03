289b6d8acf4f02eb64244dad3a8663bd7a2bf6d4
Matthew Kwong <matthewkwong2@gmail.com>
2025-01-03 20:36:07 +0800

    feat: implement my own CSS reset

--- src/components/breadcrumb.tsx
+++ src/components/breadcrumb.tsx
@@ -1,4 +1,4 @@
-import { Flex, type FlexProps, Link, Reset, Text } from '@radix-ui/themes';
+import { Flex, type FlexProps, Link, Text } from '@radix-ui/themes';
 import NextLink from 'next/link';
 import { type FC, Fragment } from 'react';
 
@@ -11,29 +11,27 @@ export interface BreadcrumbProps
 
 export const Breadcrumb: FC<BreadcrumbProps> = ({ routes, ...props }) => (
   <Flex asChild align="center" gap="2" wrap="wrap" {...props}>
-    <Reset>
-      <ul>
-        {routes.map(({ name, pathname }, index) => (
-          <Fragment key={name}>
-            <li>
-              {index === routes.length - 1 ? (
-                <Text size="2">{name}</Text>
-              ) : (
-                // eslint-disable-next-line jsx-a11y/anchor-is-valid -- wrapping next/link
-                <Link asChild color="gray" size="2">
-                  <NextLink href={pathname}>{name}</NextLink>
-                </Link>
-              )}
-            </li>
-
-            {index !== routes.length - 1 && (
-              <Text asChild color="gray" size="2">
-                <li>/</li>
-              </Text>
+    <ul>
+      {routes.map(({ name, pathname }, index) => (
+        <Fragment key={name}>
+          <li>
+            {index === routes.length - 1 ? (
+              <Text size="2">{name}</Text>
+            ) : (
+              // eslint-disable-next-line jsx-a11y/anchor-is-valid -- wrapping next/link
+              <Link asChild color="gray" size="2">
+                <NextLink href={pathname}>{name}</NextLink>
+              </Link>
             )}
-          </Fragment>
-        ))}
-      </ul>
-    </Reset>
+          </li>
+
+          {index !== routes.length - 1 && (
+            <Text asChild color="gray" size="2">
+              <li>/</li>
+            </Text>
+          )}
+        </Fragment>
+      ))}
+    </ul>
   </Flex>
 );
