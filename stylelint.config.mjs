e91aa3ae33b4ef9d87eb404bfde467fff257d6fb
Matthew Kwong <matthewkwong2@gmail.com>
2025-01-07 21:20:50 +0800

    feat: only enforce camel case CSS classes in CSS modules

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -5,14 +5,21 @@ const config = {
   extends: ["stylelint-config-standard", "stylelint-config-recess-order"],
   rules: {
     "import-notation": "string",
-    "selector-class-pattern": [
-      "^[a-z][a-zA-Z0-9]*$",
-      {
-        message: (selector) =>
-          `Expected class selector "${selector}" to be camelCase`,
-      },
-    ],
   },
+  overrides: [
+    {
+      files: ["**/*.module.css"],
+      rules: {
+        "selector-class-pattern": [
+          "^[a-z][a-zA-Z0-9]*$",
+          {
+            message: (selector) =>
+              `Expected class selector "${selector}" to be camelCase`,
+          },
+        ],
+      },
+    },
+  ],
 };
 
 export default config;
