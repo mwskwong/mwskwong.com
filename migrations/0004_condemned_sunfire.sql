8973ed7fe788cc0ad66d95f641a6a86294c7da0e
Matthew Kwong <matthewkwong2@gmail.com>
2025-01-11 22:15:13 +0800

    feat: add update blog post ID to migration SQL

--- migrations/0004_condemned_sunfire.sql
+++ migrations/0004_condemned_sunfire.sql
@@ -1,1 +1,8 @@
 ALTER TABLE "article_metadata" RENAME TO "blog_post_metadata";
+UPDATE blog_post_metadata SET id = '3MRidOWGLBv0MDtmKYlT0V' WHERE id = '1h8CnihHCfank5qq1E3AQ3';
+UPDATE blog_post_metadata SET id = '7b2gAzgCYg1LJCsmC137Op' WHERE id = '2quE5sxbwivPKyQMnPWvBs';
+UPDATE blog_post_metadata SET id = '3whZR6DnIp3aQAgdRJoPrV' WHERE id = '6AyfMHZ9FHrk37dL817DI2';
+UPDATE blog_post_metadata SET id = '1ZhLIeskZmO6T8hfgfjJko' WHERE id = '5FnWhAEkPl1xuVEgcNArLD';
+UPDATE blog_post_metadata SET id = '4Dhp1pGNfSoTtFUjdNxd43' WHERE id = '5cb9NDeFZvPF9BUQhUibty';
+UPDATE blog_post_metadata SET id = '3L6NepSWy2wfVAiiTVHDNl' WHERE id = '2Gv022Spm6DcvDRgv6Z5Vt';
+UPDATE blog_post_metadata SET id = '413bXSvacFHEzgqYvUDBKD' WHERE id = '1VUggOKJHLayXZJXwYwO4l';
