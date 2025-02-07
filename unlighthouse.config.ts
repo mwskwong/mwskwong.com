a851cd35bb9e66985a1b7ed852f898d4390a2b25
Matthew Kwong <32725014+mwskwong@users.noreply.github.com>
2025-02-07 10:04:05 +0800

    chore: update unlighthouse config to attempt to sync with pagespeed mobile config

--- unlighthouse.config.ts
+++ unlighthouse.config.ts
@@ -3,7 +3,7 @@ const config = {
   ci: {
     buildStatic: true,
     budget: {
-      performance: 95,
+      performance: 90,
       accessibility: 92, // minus point from links relying on colors to be distinguishable in /blog/:slug; info callout doesn't have enough contrast
       "best-practices": 100,
       seo: 63, // minus point from non-PROD not being indexable
@@ -11,8 +11,15 @@ const config = {
   },
   scanner: {
     samples: 3,
-    throttle: true,
     crawler: false,
+  },
+  lighthouseOptions: {
+    throttling: {
+      cpuSlowdownMultiplier: 4,
+    },
+  },
+  puppeteerClusterOptions: {
+    maxConcurrency: 1,
   },
 };
 
