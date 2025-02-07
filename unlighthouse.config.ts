ad0b9d1463fc5aea5e9458368432b5b8774f177e
Matthew Kwong <32725014+mwskwong@users.noreply.github.com>
2025-02-07 10:57:39 +0800

    chore: unlighthouse reduce performance budget

--- unlighthouse.config.ts
+++ unlighthouse.config.ts
@@ -4,7 +4,7 @@ const config = {
     reporter: "jsonExpanded",
     buildStatic: true,
     budget: {
-      performance: 90,
+      performance: 80,
       accessibility: 92, // minus point from links relying on colors to be distinguishable in /blog/:slug; info callout doesn't have enough contrast
       "best-practices": 100,
       seo: 63, // minus point from non-PROD not being indexable
