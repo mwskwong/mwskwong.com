c813ffcc836e92a97d1618b23f02659e09ec3b23
Matthew Kwong <matthewkwong2@gmail.com>
2026-03-15 06:51:12 +0800

    fix: prevent allowing `ColorSchemeToggle` `onClick` to be overridden

--- src/components/color-scheme-toggle.tsx
+++ src/components/color-scheme-toggle.tsx
@@ -3,14 +3,21 @@
 import {
   ActionIcon,
   type ActionIconProps,
+  type PolymorphicComponentProps,
   useMantineColorScheme,
 } from "@mantine/core";
 import { IconMoon, IconSun } from "@tabler/icons-react";
 
 import styles from "./color-scheme-toggle.module.css";
 
-export type ColorSchemeToggleProps = Omit<ActionIconProps, "children">;
-export const ColorSchemeToggle = (props: ColorSchemeToggleProps) => {
+export type ColorSchemeToggleProps = Omit<
+  PolymorphicComponentProps<"button", ActionIconProps>,
+  "children"
+>;
+export const ColorSchemeToggle = ({
+  onClick,
+  ...props
+}: ColorSchemeToggleProps) => {
   const { toggleColorScheme } = useMantineColorScheme();
 
   return (
@@ -18,7 +25,10 @@ export const ColorSchemeToggle = (props: ColorSchemeToggleProps) => {
       aria-label="Toggle color scheme"
       color="dark"
       variant="subtle"
-      onClick={toggleColorScheme}
+      onClick={(event) => {
+        toggleColorScheme();
+        onClick?.(event);
+      }}
       {...props}
     >
       <IconSun className={styles.light} size={18} />
