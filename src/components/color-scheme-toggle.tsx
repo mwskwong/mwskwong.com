28107d967613523c272f9278fe62fe182d98a563
Matthew Kwong <matthew.ws.kwong@hkjc.org.hk>
2026-03-18 10:30:38 +0800

    feat: change back to hard code `c=teal` for text just to keep it simple. And move import fo `global.css` to root layout

--- src/components/color-scheme-toggle.tsx
+++ src/components/color-scheme-toggle.tsx
@@ -4,17 +4,11 @@ import {
   ActionIcon,
   type ActionIconProps,
   type ElementProps,
-  Title as MantineTitle,
-  type TitleProps as MantineTitleProps,
-  Text,
   useMantineColorScheme,
-  useMantineTheme,
 } from "@mantine/core";
 import { IconMoon, IconSun } from "@tabler/icons-react";
 
-import { firstName, lastName } from "@/config";
-
-import styles from "./client-components.module.css";
+import styles from "./color-scheme-toggle.module.css";
 
 export type ColorSchemeToggleProps = ActionIconProps &
   ElementProps<"button", keyof ActionIconProps | "children">;
@@ -35,22 +29,8 @@ export const ColorSchemeToggle = ({
       }}
       {...props}
     >
-      <IconSun className={styles.colorSchemeToggleIconLight} size={18} />
-      <IconMoon className={styles.colorSchemeToggleIconDark} size={18} />
+      <IconSun className={styles.iconLight} size={18} />
+      <IconMoon className={styles.iconDark} size={18} />
     </ActionIcon>
-  );
-};
-
-export type TitleProps = Omit<MantineTitleProps, "children">;
-export const Title = (props: TitleProps) => {
-  const { primaryColor } = useMantineTheme();
-
-  return (
-    <MantineTitle {...props}>
-      <Text c={primaryColor} inherit span>
-        {firstName}
-      </Text>{" "}
-      {lastName}
-    </MantineTitle>
   );
 };
