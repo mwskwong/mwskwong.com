08f71977ad18f54df50f447e9d97daa801c8833e
Matthew Kwong <matthew.ws.kwong@hkjc.org.hk>
2026-03-23 18:05:47 +0800

    feat: use `birch`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,15 @@ import {
   MantineProvider,
   mantineHtmlProps,
 } from "@mantine/core";
+import {
+  type DefaultMantineColor,
+  type MantineColorsTuple,
+  createTheme,
+} from "@mantine/core";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { type Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 
 import {
   description,
@@ -19,7 +25,40 @@ import {
   siteName,
 } from "@/config";
 
-import { theme } from "./theme";
+declare module "@mantine/core" {
+  export interface MantineThemeColorsOverride {
+    colors: Record<
+      "primary" | "walnut" | "birch" | DefaultMantineColor,
+      MantineColorsTuple
+    >;
+  }
+}
+
+const geist = Geist({ subsets: ["latin"] });
+const geistMono = Geist_Mono({ subsets: ["latin"], preload: false });
+
+const theme = createTheme({
+  fontFamily: geist.style.fontFamily,
+  fontFamilyMonospace: geistMono.style.fontFamily,
+  primaryColor: "primary",
+  primaryShade: { light: 9, dark: 5 },
+  colors: {
+    // wood accent
+    primary: [
+      "#fff3e5",
+      "#f9e5d4",
+      "#eecaac",
+      "#e0a472",
+      "#da955b",
+      "#d58543",
+      "#d37d35",
+      "#bb6b28",
+      "#a75e20",
+      "#925016",
+    ],
+  },
+  respectReducedMotion: true,
+});
 
 export const metadata: Metadata = {
   title: `${firstName} ${lastName} · ${displayTitle}`,
