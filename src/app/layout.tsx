cef95f489de84eff9fa0828f04fd29975e7d747c
Matthew Kwong <matthewkwong2@gmail.com>
2026-03-23 22:12:28 +0800

    refactor: override CSS var in `cssVariablesResolver`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,9 @@
-import "@mantine/core/styles.css";
 import "./global.css";
 
-import {
-  ColorSchemeScript,
-  MantineProvider,
-  mantineHtmlProps,
-} from "@mantine/core";
-import {
-  type DefaultMantineColor,
-  type MantineColorsTuple,
-  createTheme,
-} from "@mantine/core";
+import { ColorSchemeScript, mantineHtmlProps } from "@mantine/core";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { type Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 
 import {
   description,
@@ -25,40 +14,7 @@ import {
   siteName,
 } from "@/config";
 
-declare module "@mantine/core" {
-  export interface MantineThemeColorsOverride {
-    colors: Record<
-      "primary" | "walnut" | "birch" | DefaultMantineColor,
-      MantineColorsTuple
-    >;
-  }
-}
-
-const geist = Geist({ subsets: ["latin"] });
-const geistMono = Geist_Mono({ subsets: ["latin"], preload: false });
-
-const theme = createTheme({
-  fontFamily: geist.style.fontFamily,
-  fontFamilyMonospace: geistMono.style.fontFamily,
-  primaryColor: "primary",
-  primaryShade: { light: 9, dark: 3 },
-  colors: {
-    // wood accent
-    primary: [
-      "#fff3e5",
-      "#f9e5d4",
-      "#eecaac",
-      "#e0a472",
-      "#da955b",
-      "#d58543",
-      "#d37d35",
-      "#bb6b28",
-      "#a75e20",
-      "#925016",
-    ],
-  },
-  respectReducedMotion: true,
-});
+import { Providers } from "./providers";
 
 export const metadata: Metadata = {
   title: `${firstName} ${lastName} · ${displayTitle}`,
@@ -74,9 +30,7 @@ const RootLayout = ({ children }: LayoutProps<"/">) => (
       <ColorSchemeScript defaultColorScheme="auto" />
     </head>
     <body>
-      <MantineProvider defaultColorScheme="auto" theme={theme}>
-        {children}
-      </MantineProvider>
+      <Providers defaultColorScheme="auto">{children}</Providers>
       <Analytics />
       <SpeedInsights />
     </body>
---
+++ src/app/providers.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import "@mantine/core/styles.css";
+
+import {
+  type CSSVariablesResolver,
+  type DefaultMantineColor,
+  type MantineColorsTuple,
+  MantineProvider,
+  type MantineProviderProps,
+  createTheme,
+} from "@mantine/core";
+import { Geist, Geist_Mono } from "next/font/google";
+
+declare module "@mantine/core" {
+  export interface MantineThemeColorsOverride {
+    colors: Record<"primary" | DefaultMantineColor, MantineColorsTuple>;
+  }
+}
+
+const geist = Geist({ subsets: ["latin"] });
+const geistMono = Geist_Mono({ subsets: ["latin"], preload: false });
+
+const theme = createTheme({
+  fontFamily: geist.style.fontFamily,
+  fontFamilyMonospace: geistMono.style.fontFamily,
+  primaryColor: "primary",
+  primaryShade: { light: 9, dark: 3 },
+  colors: {
+    // wood accent
+    primary: [
+      "#fff3e5",
+      "#f9e5d4",
+      "#eecaac",
+      "#e0a472",
+      "#da955b",
+      "#d58543",
+      "#d37d35",
+      "#bb6b28",
+      "#a75e20",
+      "#925016",
+    ],
+  },
+  respectReducedMotion: true,
+});
+
+const cssVariablesResolver: CSSVariablesResolver = (theme) => {
+  const primaryShadeDark =
+    typeof theme.primaryShade === "number"
+      ? theme.primaryShade
+      : theme.primaryShade.dark;
+  return {
+    variables: {},
+    light: {},
+    dark: {
+      "--mantine-color-body": theme.colors.dark[9],
+      "--mantine-color-primary-text":
+        theme.colors[theme.primaryColor][primaryShadeDark],
+      " --mantine-color-anchor":
+        theme.colors[theme.primaryColor][primaryShadeDark],
+    },
+  };
+};
+
+export type ProvidersProps = Omit<
+  MantineProviderProps,
+  "theme" | "cssVariablesResolver"
+>;
+export const Providers = (props: ProvidersProps) => (
+  <MantineProvider
+    cssVariablesResolver={cssVariablesResolver}
+    theme={theme}
+    {...props}
+  />
+);
