daef5e80cf3e1173dcee8c73182fa6e73dccf66d
Matthew Kwong <matthewkwong2@gmail.com>
2026-04-03 17:59:12 +0800

    refactor: rename var

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const firstName = "Matthew";
 export const middleName = "Wang Shun";
 export const lastName = "Kwong";
 
-export const displayTitle = "Tech Lead — Full Stack Web Developer";
+export const headline = "Tech Lead — Full Stack Web Developer";
 export const currentRole = {
   jobTitle: "Technical Manager",
   company: { name: "The Hong Kong Jockey Club", url: "https://www.hkjc.com" },
