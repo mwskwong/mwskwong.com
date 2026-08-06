7b9dcced76b3756b8747735184813aa438ad113b
Matthew Kwong <matthewkwong2@gmail.com>
2026-08-06 21:53:32 +0800

    feat: update oxfmt config

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -1,21 +1,12 @@
 import { defineConfig } from "oxfmt";
 
 const oxfmtConfig = defineConfig({
-  sortImports: {
-    groups: [
-      "type-import",
-      ["value-builtin", "value-external"],
-      "type-internal",
-      "value-internal",
-      ["type-parent", "type-sibling", "type-index"],
-      ["value-parent", "value-sibling", "value-index"],
-      "unknown",
-    ],
-  },
+  sortImports: true,
   sortTailwindcss: {
     stylesheet: "./src/app/globals.css",
     functions: ["cn"],
   },
+  sortPackageJson: { sortScripts: true },
 });
 
 export default oxfmtConfig;
