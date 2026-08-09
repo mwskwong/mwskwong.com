1391c43d381d9be011562758e2c2dddfe4a5b914
Matthew Kwong <matthewkwong2@gmail.com>
2026-08-09 10:16:24 +0800

    chore: update `ultracite`

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -4,8 +4,8 @@ import ultracite from "ultracite/oxfmt";
 const oxfmtConfig = defineConfig({
   ...ultracite,
   sortTailwindcss: {
+    functions: ["clsx", "cva", "tw", "twMerge", "cn", "twJoin", "tv"],
     stylesheet: "./src/app/globals.css",
-    functions: ["cn"],
   },
 });
 
